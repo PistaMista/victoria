835d740e758dac66c41738c7afc1d599dfd59d27
Krystof Albrecht <krystofalbrechtus@gmail.com>
2024-09-06 13:15:05 +0200

    Add flake.nix for Python applications

--- flake.nix
+++ flake.nix
@@ -2,10 +2,27 @@
 	description = "My testing Python app with Flakes";
 	inputs = {
 		nixpkgs.url =  "github:NixOS/nixpkgs/release-24.05";
+		poetry2nix.url = "github:nix-community/poetry2nix";
+		flake-utils.url = "github:numtide/flake-utils";
 	};
 
-	outputs = {self, nixpkgs }: {
-		defaultPackages.x86_64-linux = with import nixpkgs { system = "x86_64-linux"; }; hello;
-
-	};
+	outputs = {self, nixpkgs, poetry2nix, flake-utils }: 
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = nixpkgs.legacyPackages.${system};
+				packageName = "veronica";
+				inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+				app = mkPoetryApplication {
+					projectDir = ./.;
+				};
+			in
+			{
+				packages.${packageName} = app;
+				defaultPackage = self.packages.${system}.${packageName};
+				devShell = pkgs.mkShell {
+					buildInputs = with pkgs; [ poetry ];
+					inputsFrom = builtins.attrValues self.packages.${system};
+				};
+			}
+		);
 }
