3eef36322de42711bf5a9bfe56d266e3fd9792b3
Krystof Albrecht <krystofalbrechtus@gmail.com>
2024-09-06 20:46:55 +0200

    Test basic LLM

--- victoria/backend/__init__.py
+++ victoria/backend/__init__.py
@@ -1,4 +1,21 @@
-import langchain
+from langchain_ollama import ChatOllama
+from langchain_core.messages import AIMessage
+
 
 def main():
-    print("hello world!")
+    llm = ChatOllama(
+            model = "dolphin-llama3"
+        )
+
+    messages = [
+                (
+                    "system",
+                    "You are a helpful assistant that translates English to French. Translate the user sentence.",
+                ),
+                ("human", "I love programming"),
+            ]
+
+    ai_msg = llm.invoke(messages)
+    print(ai_msg)
+
+
