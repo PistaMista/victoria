9cfc080a63c0cc7f69db1627591c98189b1b1e81
Krystof Albrecht <krystofalbrechtus@gmail.com>
2024-09-10 01:32:12 +0200

    Fix serving static files

--- victoria-backend/victoria_backend/__init__.py
+++ victoria-backend/victoria_backend/__init__.py
@@ -3,9 +3,9 @@ from langchain_core.messages import AIMessage
 from flask import Flask, send_from_directory
 from argparse import ArgumentParser
 from waitress import serve
-from os import environ
+import os
 
-app = Flask(__name__, static_folder=environ['FRONTEND_PATH'] or '../../victoria-frontend/build')
+app = Flask(__name__, static_folder=os.environ['FRONTEND_PATH'] or '../../victoria-frontend/build')
 parser = ArgumentParser(
             prog='Victoria',
             description='An all-purpose AI assistant')
@@ -13,8 +13,15 @@ parser.add_argument('-a', '--host-address')
 parser.add_argument('-p', '--port')
 
 @app.route("/")
-def frontend():
+def index():
     return send_from_directory(app.static_folder, 'index.html')
+
+@app.route("/<path:path>")
+def static_files(path):
+    if os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)   
+    else:
+        return index()
 
 def main():
     args = parser.parse_args()
