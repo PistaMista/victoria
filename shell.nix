cfcf0efc92cb06c7a074e289933d5b1619c2d306
Krystof Albrecht <krystofalbrechtus@gmail.com>
2024-09-09 21:54:55 +0200

    Add VSCode to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,31 @@
 let
-pkgs = import (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") { };
+	config = {
+		allowUnfree = true;
+	};
+	pkgs = import (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") { inherit config; };
+	vscode = pkgs.vscode-with-extensions.override {
+		vscodeExtensions = with pkgs.vscode-extensions;
+		[
+			eamodio.gitlens
+			nonylene.dark-molokai-theme
+			asvetliakov.vscode-neovim
+			usernamehw.errorlens
+			svelte.svelte-vscode
+			ms-python.python
+		]
+		++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+			{
+				name = "git-graph";
+				publisher = "mhutchie";
+				version = "1.30.0";
+				sha256 = "sHeaMMr5hmQ0kAFZxxMiRk6f0mfjkg2XMnA4Gf+DHwA=";
+			}
+		];
+	};
 in pkgs.mkShell {
 	packages = [
 		pkgs.poetry
 		pkgs.nodejs_22
+		vscode
 	];
 }
