100b5d9020936ca83dc7aa7de774028b7c9c8d9c
Krystof Albrecht <krystofalbrechtus@gmail.com>
2024-09-11 14:26:42 +0200

    Style the ChatWindow

--- victoria-frontend/src/lib/ChatWindow.svelte
+++ victoria-frontend/src/lib/ChatWindow.svelte
@@ -1,5 +1,16 @@
+<script>
+    export let reply = "";
+</script>
+
 <div class="p-2 w-full h-full">
-    <div class="p-2 w-full h-full text-center bg-red-500">
-        Chat Window
+    <div 
+        class="
+            p-2 w-full h-full 
+            overflow-y-scroll 
+            bg-gray-200 border border-slate-600 
+            focus:border-primary-900
+            shadow-inner rounded-lg"
+    >
+        {reply}
     </div>
 </div>
