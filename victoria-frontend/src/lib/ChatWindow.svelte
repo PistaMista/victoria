d6044232abc07ab597e4dbf9da6a9128cefd7366
Krystof Albrecht <krystofalbrechtus@gmail.com>
2024-09-12 01:50:48 +0200

    Scroll the ChatWindow to the latest message

--- victoria-frontend/src/lib/ChatWindow.svelte
+++ victoria-frontend/src/lib/ChatWindow.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
+    import { afterUpdate } from "svelte";
     import AssistantMessage from "./ChatItem/AssistantMessage.svelte";
     import UserMessage from "./ChatItem/UserMessage.svelte";
     
     export let messages: Array<any> = [];
+    
+    let container: HTMLDivElement;
+
+    // Scroll to the last message when the container element changes
+    afterUpdate(() => {
+        container.scrollTo(0, container.scrollHeight);
+    })
 </script>
 
 <div class="p-2 w-full h-full">
     <div class="p-2 h-full bg-gray-200 border border-slate-600 focus:border-primary-900 shadow-inner rounded-lg">
-        <div class="p-2 h-full overflow-y-auto">
+        <div bind:this={container} class="p-2 h-full overflow-y-auto scroll-smooth">
             {#each messages as message}
                 {#if message.type == 'user'}
                     <UserMessage> {message.content} </UserMessage>
