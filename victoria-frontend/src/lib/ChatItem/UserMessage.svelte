8bfbf8dbc5482437d7dfcf2a7cab017eddb6b462
Krystof Albrecht <krystofalbrechtus@gmail.com>
2024-09-12 19:32:46 +0200

    FIX TEXT!

--- victoria-frontend/src/lib/ChatItem/UserMessage.svelte
+++ victoria-frontend/src/lib/ChatItem/UserMessage.svelte
@@ -1,5 +1,5 @@
 <div class="p-1 flex flex-row justify-end">
-    <div class="p-2 bg-primary-900 text-slate-600 rounded-lg">
+    <div class="p-2 bg-primary-900 text-slate-200 rounded-lg">
         <slot/>
     </div>
 </div>
