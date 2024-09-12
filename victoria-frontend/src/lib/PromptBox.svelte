d52542547cbb687dbeb268a64de48adea5932ef9
Krystof Albrecht <krystofalbrechtus@gmail.com>
2024-09-12 15:02:09 +0200

    Add show_spinner to PromptBox

--- victoria-frontend/src/lib/PromptBox.svelte
+++ victoria-frontend/src/lib/PromptBox.svelte
@@ -1,14 +1,15 @@
 <script>
-    import { Input, Button, ButtonGroup } from 'flowbite-svelte';
+    import { Input, Button, ButtonGroup, Spinner } from 'flowbite-svelte';
     import { PaperPlaneOutline } from 'flowbite-svelte-icons';
     
     export let prompt = "";
     export let autofocus = false;
+    export let show_spinner = false;
     export let on_submit = () => {};
 </script>
 
 <div class="m-2 p-2">
-    <form on:submit={on_submit}>
+    <form on:submit={() => { if (!show_spinner) { on_submit() }}}>
         <ButtonGroup class="w-full">
             {#if autofocus}
                 <Input 
@@ -24,8 +25,12 @@
                     bind:value={prompt} 
                 />
             {/if}
-            <Button color="primary" disabled={prompt.length == 0} type="submit">
-                <PaperPlaneOutline/>
+            <Button color="primary" disabled={prompt.length == 0 || show_spinner} type="submit">
+                {#if show_spinner}
+                    <Spinner size="4"/>
+                {:else}
+                    <PaperPlaneOutline/>
+                {/if}
             </Button>
         </ButtonGroup>
     </form>
