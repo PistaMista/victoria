1a938958c05e925ebef8699db1c4f36f36041157
Krystof Albrecht <krystofalbrechtus@gmail.com>
2024-09-15 00:04:45 +0200

    Add ability to edit previously sent messages

--- victoria-frontend/src/lib/ChatItem/ContextMenuBase.svelte
+++ victoria-frontend/src/lib/ChatItem/ContextMenuBase.svelte
@@ -1,3 +1,16 @@
-<div class="p-1 -mt-2 bg-slate-500 rounded-lg">
-    MENU!
-</div>
+<script lang="ts">
+    import { ButtonGroup, Button } from "flowbite-svelte";
+    import type { ComponentType } from "svelte";
+    type ContextAction = {
+        icon_component: ComponentType,
+        action: () => void
+    }
+    
+    export let context_actions: Array<ContextAction> = [];
+</script>
+
+<ButtonGroup class="-mt-2 bg-slate-500 text-slate-200">
+    {#each context_actions as action}
+        <Button on:click={action.action}> <svelte:component this={action.icon_component}/> </Button>
+    {/each}
+</ButtonGroup>
--- victoria-frontend/src/lib/ChatItem/Messages/UserMessage.svelte
+++ victoria-frontend/src/lib/ChatItem/Messages/UserMessage.svelte
@@ -2,16 +2,36 @@
     import { marked } from 'marked';
     import ChatItemBase from '../ChatItemBase.svelte';
     import ContextMenuBase from '../ContextMenuBase.svelte';
+    import { EditOutline } from 'flowbite-svelte-icons';
+    import { Textarea } from 'flowbite-svelte';
+
     export let content = "";
+    let edit_mode = false;
 </script>
 
 <ChatItemBase justify_end={true} inner_class="bg-primary-900">
     <svelte:fragment slot="content">
-        <!-- FIXME: There is totally an XSS attack here -->
-        {@html marked(content)}
+        {#if !edit_mode}
+            <!-- FIXME: There is totally an XSS attack here -->
+            {@html marked(content)}
+        {:else}
+            <Textarea
+                class="rounded-lg"
+                bind:value={content}
+                on:blur={() => { edit_mode = false; }}
+                autofocus
+            />
+        {/if}
     </svelte:fragment>
     
     <svelte:fragment slot="menu">
-        <ContextMenuBase/>
+        <ContextMenuBase
+            context_actions={[
+                {
+                    icon_component: EditOutline,
+                    action: () => { edit_mode = !edit_mode; }
+                }
+            ]}
+        />
     </svelte:fragment>
 </ChatItemBase>
