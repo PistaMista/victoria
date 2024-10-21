615bc4c649222e4eb453551d0af8fe3ab7e9d2ca
Krystof Albrecht <krystofalbrechtus@gmail.com>
2024-10-21 15:49:20 +0200

    Reenable web search

--- victoria-backend/victoria_backend/tools/web_search.py
+++ victoria-backend/victoria_backend/tools/web_search.py
@@ -1,18 +1,18 @@
 from langchain.agents import tool
 from duckduckgo_search import DDGS
 
-# @tool
-# def web_search(query: str):
-#     """Searches the web for the given search term,
-#     returning an array of dictionaries of the form:
-#     {
-#         'title': RESULT_TITLE,
-#         'href': LINK_TO_THE_WEBPAGE,
-#         'body': DESCRIPTION
-#     }
+@tool
+def web_search(query: str):
+    """Searches the web for the given search term,
+    returning an array of dictionaries of the form:
+    {
+        'title': RESULT_TITLE,
+        'href': LINK_TO_THE_WEBPAGE,
+        'body': DESCRIPTION
+    }
     
-#     When asked about a location, try searching in the language spoken
-#     at the place you are asked about.
-#     """
-#     results = DDGS().text(query, max_results=10)
-#     return results
+    When asked about a location, try searching in the language spoken
+    at the place you are asked about.
+    """
+    results = DDGS().text(query, max_results=10)
+    return results
