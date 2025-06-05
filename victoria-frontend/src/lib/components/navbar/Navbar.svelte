71139c820658ea6ea73783f0d57b8b8ea404158e
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-05 23:13:52 +0200

    feat: add responsiveness to navbar

--- victoria-frontend/src/lib/components/navbar/Navbar.svelte
+++ victoria-frontend/src/lib/components/navbar/Navbar.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
     import type { Route } from "$lib/types/route";
     import NavbarItem from "./NavbarItem.svelte";
+    import Button from "../button/Button.svelte";
+    import { BarsOutline } from "flowbite-svelte-icons";
     
     export let routes: Array<Route>;
 </script>
 
-<nav class="flex w-full bg-slate-300 border-b-2 border-orange-400">
-    {#each routes as route}
-        <NavbarItem {route}/>
-    {/each}
-</nav>
+<div class="flex bg-slate-300 border-b-2 border-orange-400">
+    <nav class="hidden md:flex w-full border-inherit">
+        {#each routes as route}
+            <NavbarItem {route}/>
+        {/each}
+    </nav>
+
+    <Button class="md:hidden hover:bg-slate-400 px-1">
+        <BarsOutline/>
+    </Button>
+</div>
+
