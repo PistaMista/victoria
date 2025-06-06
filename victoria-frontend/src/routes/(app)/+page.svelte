570b4cfc48ba18ff32bb25394299794a6350e4a7
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-06 12:50:11 +0200

    feat: add more widgets to dashboard wireframe

--- victoria-frontend/src/routes/(app)/+page.svelte
+++ victoria-frontend/src/routes/(app)/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-    import { BanOutline, CheckCircleOutline, ClockOutline } from "flowbite-svelte-icons";
+    import { BanOutline, CheckCircleOutline, CheckOutline, ClockOutline, EnvelopeOutline, ExclamationCircleOutline, MailBoxOutline } from "flowbite-svelte-icons";
 
 </script>
 
 
-<div class="flex justify-center p-2">
-    <div class="flex flex-col">
+<div class="flex flex-wrap justify-center p-2">
+    <div class="flex flex-col m-1">
         <div class="flex justify-center text-xl font-bold">
             Monologue Activity
         </div>
@@ -24,6 +24,105 @@
                 Generate insight
                 <div class="grow mx-2"/>
                 <BanOutline/>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex flex-col m-1">
+        <div class="flex justify-center text-xl font-bold">
+            Pending Tasks
+        </div>
+        <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                Collect news
+                <div class="grow mx-2"/>
+                <ExclamationCircleOutline/>
+            </div>
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                Research thesis ideas
+                <div class="grow mx-2"/>
+                <CheckCircleOutline/>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex flex-col m-1">
+        <div class="flex justify-center text-xl font-bold">
+            Agent Status
+        </div>
+        <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                Researcher
+                <div class="grow mx-2"/>
+                <CheckOutline/>
+            </div>
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                Sysadmin
+                <div class="grow mx-2"/>
+                <ExclamationCircleOutline/>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col m-1">
+        <div class="flex justify-center text-xl font-bold">
+            Unread Messages
+        </div>
+        <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                Failed system units
+                <div class="grow mx-2"/>
+                <EnvelopeOutline/>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col m-1">
+        <div class="flex justify-center text-xl font-bold">
+            Other Widget
+        </div>
+        <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                Researcher
+                <div class="grow mx-2"/>
+                <CheckOutline/>
+            </div>
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                Sysadmin
+                <div class="grow mx-2"/>
+                <ExclamationCircleOutline/>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col m-1">
+        <div class="flex justify-center text-xl font-bold">
+            Other Widget
+        </div>
+        <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                Researcher
+                <div class="grow mx-2"/>
+                <CheckOutline/>
+            </div>
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                Sysadmin
+                <div class="grow mx-2"/>
+                <ExclamationCircleOutline/>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col m-1">
+        <div class="flex justify-center text-xl font-bold">
+            Other Widget
+        </div>
+        <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                asdasdasd
+                <div class="grow mx-2"/>
+                <CheckOutline/>
+            </div>
+            <div class="flex bg-slate-400 p-1 rounded-md">
+                Sysadmin
+                <div class="grow mx-2"/>
+                <ExclamationCircleOutline/>
             </div>
         </div>
     </div>
