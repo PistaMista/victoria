ff441c65b1b64d9eac55644341610deca8873d39
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-10 23:43:40 +0200

    feat: wireframe for agent editing view

---
+++ victoria-frontend/src/routes/(app)/agents/[id]/+page.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+    import { Textarea, Button, Range, Input, Label, Toggle } from "flowbite-svelte";
+    import { RefreshOutline } from "flowbite-svelte-icons";
+</script>
+
+<div class="w-full h-full overflow-y-auto">
+    <div class="flex flex-row">
+        <div class="md:basis-1/5"/>
+        <div class="grow md:basis-3/5 flex flex-col p-2">
+            <div class="font-bold">
+                Researcher
+            </div>
+            <div class="my-2">
+                <Label class="font-bold">Thumbnail</Label>
+                <div class="bg-slate-400 rounded-md p-2">
+                    <div class="place-self-center">
+                        <div class="w-48 h-48 bg-yellow-200 rounded-md"></div>
+                        <Button class="m-2">Pick image</Button>
+                    </div>
+                </div>
+            </div>
+            <div class="my-2">
+                <Label class="font-bold">System prompt</Label>
+                <div class="rounded-md bg-slate-400 p-2">
+                    <Textarea/>
+                </div>
+            </div>
+            <div class="my-2">
+                <Label class="font-bold">Model parameters</Label>
+                <div class="rounded-md bg-slate-400 p-2">
+                    <div class="flex flex-row mt-2">
+                        <div class="grow flex flex-col">
+                            <Label>Temperature</Label>
+                            <Range class="place-self-center ml-1 grow"/>
+                        </div>
+                        <Input value={20} class="place-self-center ml-1 w-12"/>
+                        <Button class="p-1 ml-1"><RefreshOutline></RefreshOutline></Button>
+                    </div>
+                    <div class="flex flex-row mt-2">
+                        <div class="grow flex flex-col">
+                            <Label>Low-k</Label>
+                            <Range class="place-self-center ml-1 grow"/>
+                        </div>
+                        <Input value={20} class="place-self-center ml-1 w-12"/>
+                        <Button class="p-1 ml-1"><RefreshOutline></RefreshOutline></Button>
+                    </div>
+                    <div class="flex flex-row mt-2">
+                        <div class="grow flex flex-col">
+                            <Label>Tokens To Keep On Context Refresh</Label>
+                            <Range class="place-self-center ml-1 grow"/>
+                        </div>
+                        <Input value={20} class="place-self-center ml-1 w-12"/>
+                        <Button class="p-1 ml-1"><RefreshOutline></RefreshOutline></Button>
+                    </div>
+
+                </div>
+            </div>
+            <div class="my-2">
+                <Label class="font-bold"> Triggers </Label>
+                <div class="bg-slate-400 rounded-md p-2">
+                    <div class="flex flex-row m-1 bg-slate-200 rounded-md p-1">
+                        <div class="font-bold grow">
+                            Chat
+                        </div>
+                        <Toggle/>
+                    </div>
+                    <div class="flex flex-row m-1 bg-slate-200 rounded-md p-1">
+                        <div class="font-bold grow">
+                            Timer
+                        </div>
+                        <Toggle/>
+                    </div>
+                    <div class="flex flex-row m-1 bg-slate-200 rounded-md p-1">
+                        <div class="font-bold grow">
+                            Task passing due date
+                        </div>
+                        <Toggle/>
+                    </div>
+                </div>
+            </div>
+            <div class="my-2">
+                <Label class="font-bold"> Actions </Label>
+                <div class="bg-slate-400 rounded-md p-2">
+                    <div class="flex flex-row m-1 bg-slate-200 rounded-md p-1">
+                        <div class="font-bold grow">
+                            Send chat message
+                        </div>
+                        <Toggle/>
+                    </div>
+                    <div class="flex flex-row m-1 bg-slate-200 rounded-md p-1">
+                        <div class="font-bold grow">
+                            Search web
+                        </div>
+                        <Toggle/>
+                    </div>
+                    <div class="flex flex-row m-1 bg-slate-200 rounded-md p-1">
+                        <div class="font-bold grow">
+                            Access task list
+                        </div>
+                        <Toggle/>
+                    </div>
+                    <div class="flex flex-row m-1 bg-slate-200 rounded-md p-1">
+                        <div class="font-bold grow">
+                            Access Zettelkasten
+                        </div>
+                        <Toggle/>
+                    </div>
+                    <div class="flex flex-row m-1 bg-slate-200 rounded-md p-1">
+                        <div class="font-bold grow">
+                            Use memory
+                        </div>
+                        <Toggle/>
+                    </div>
+                </div>
+            </div>
+            <div class="my-3">
+                <Button>Save changes</Button>
+            </div>
+        </div>
+        <div class="md:basis-1/5"/>
+    </div>
+</div>
