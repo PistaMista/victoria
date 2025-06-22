01cf0ca19c501ae5412eed5bd5f37b07aacc535f
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-22 14:08:34 +0200

    feat: wireframe for /admin/settings

---
+++ victoria-frontend/src/routes/(app)/admin/settings/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+    import { Label } from "flowbite-svelte";
+    import { LinkOutline, CogSolid, BrainSolid, ChevronRightOutline } from "flowbite-svelte-icons";
+</script>
+
+<div class="w-full h-full overflow-y-auto">
+    <div class="flex flex-col content-center mx-2 md:m-auto md:max-w-96">
+        <div class="my-2">
+            <!-- This is a DetailViewSection -->
+            <Label class="sticky top-0 bg-white px-2 z-40 font-bold">Settings</Label>
+            <div class="bg-slate-400 rounded-md p-2">
+                <div class="flex flex-col space-y-2">
+                    <!-- This is an OptionsRouteButton -->
+                    <div class="flex flex-row w-full hover:bg-slate-500 p-1 rounded-md">
+                        <div class="rounded-full bg-black p-1">
+                            <CogSolid class="w-6 h-6 text-orange-500"/>
+                        </div>
+                        <div class="ml-2 content-center">
+                            General
+                        </div>
+                        <ChevronRightOutline class="h-8 w-8 my-auto ml-auto"/>
+                    </div>
+
+                    <!-- This is an OptionsRouteButton -->
+                    <div class="flex flex-row w-full hover:bg-slate-500 p-1 rounded-md">
+                        <div class="rounded-full bg-black p-1">
+                            <LinkOutline class="w-6 h-6 text-purple-500"/>
+                        </div>
+                        <div class="ml-2 content-center">
+                            Connections
+                        </div>
+                        <ChevronRightOutline class="h-8 w-8 my-auto ml-auto"/>
+                    </div>
+
+                    <!-- This is an OptionsRouteButton -->
+                    <div class="flex flex-row w-full hover:bg-slate-500 p-1 rounded-md">
+                        <div class="rounded-full bg-black p-1">
+                            <BrainSolid class="w-6 h-6 text-white"/>
+                        </div>
+                        <div class="ml-2 content-center">
+                            Models
+                        </div>
+                        <ChevronRightOutline class="h-8 w-8 my-auto ml-auto"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
