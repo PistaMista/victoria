963cfe0e05057cea09b54e47ac5a9f33a10dc17b
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-26 22:33:48 +0200

    feat: add message recipient to chat options wireframe

--- victoria-frontend/src/routes/(app)/chats/[id]/options/+page.svelte
+++ victoria-frontend/src/routes/(app)/chats/[id]/options/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-    import {  Button, Range, Input, Label, Toggle } from "flowbite-svelte";
-    import { RefreshOutline } from "flowbite-svelte-icons";
+    import {  Button, Range, Input, Label, Toggle, Dropdown, DropdownItem } from "flowbite-svelte";
+    import { ChevronDownOutline } from "flowbite-svelte-icons";
 </script>
 
 <div class="w-full h-full overflow-y-auto">
     <div class="flex flex-col content-center mx-2 md:m-auto md:max-w-96">
+        <div class="my-2">
+            <!-- This is a DetailViewSection -->
+            <!-- This section is the same as the Agent Detail's Actions section -->
+            <Label class="sticky top-0 bg-white px-2 z-40 font-bold">Message recipient</Label>
+            <div class="bg-slate-400 rounded-md p-2">
+                <Button class="flex flex-row w-full">General<ChevronDownOutline class="w-6 h-6"/></Button>
+                <Dropdown>
+                    <DropdownItem>General</DropdownItem>
+                    <DropdownItem>Technical</DropdownItem>
+                    <DropdownItem>Research</DropdownItem>
+                </Dropdown>
+            </div>
+        </div>
         <div class="my-2">
             <!-- This is a DetailViewSection -->
             <!-- This section is the same as the Agent Detail's Actions section -->
@@ -39,37 +52,6 @@
                             Use memory
                         </div>
                         <Toggle/>
-                    </div>
-            </div>
-        </div>
-        <div class="my-2">
-            <!-- This is a DetailViewSection -->
-            <!-- This section is the same as the Agent Detail's Model parameters section -->
-            <Label class="sticky top-0 bg-white px-2 z-40 font-bold">Model parameters</Label>
-            <div class="bg-slate-400 rounded-md p-2">
-                    <div class="flex flex-row mt-2">
-                        <div class="grow flex flex-col">
-                            <Label>Temperature</Label>
-                            <Range class="place-self-center ml-1 grow"/>
-                        </div>
-                        <Input value={20} class="place-self-center ml-1 w-12"/>
-                        <Button class="p-1 ml-1"><RefreshOutline></RefreshOutline></Button>
-                    </div>
-                    <div class="flex flex-row mt-2">
-                        <div class="grow flex flex-col">
-                            <Label>Low-k</Label>
-                            <Range class="place-self-center ml-1 grow"/>
-                        </div>
-                        <Input value={20} class="place-self-center ml-1 w-12"/>
-                        <Button class="p-1 ml-1"><RefreshOutline></RefreshOutline></Button>
-                    </div>
-                    <div class="flex flex-row mt-2">
-                        <div class="grow flex flex-col">
-                            <Label>Tokens To Keep On Context Refresh</Label>
-                            <Range class="place-self-center ml-1 grow"/>
-                        </div>
-                        <Input value={20} class="place-self-center ml-1 w-12"/>
-                        <Button class="p-1 ml-1"><RefreshOutline></RefreshOutline></Button>
                     </div>
             </div>
         </div>
