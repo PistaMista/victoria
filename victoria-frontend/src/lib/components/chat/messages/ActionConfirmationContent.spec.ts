291f0ad81435eb267fef146233f88615d7a6bcc2
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-27 21:01:03 +0200

    feat: add spec to ActionConfirmationContent

--- victoria-frontend/src/lib/components/chat/messages/ActionConfirmationContent.spec.ts
+++ victoria-frontend/src/lib/components/chat/messages/ActionConfirmationContent.spec.ts
@@ -7,3 +7,5 @@ test.todo('action confirmation message shows details of invocation with given id
 test.todo('pressing "execute" in action confirmation message sends "true" as a response to the invocation query')
 
 test.todo('pressing "abort" in action confirmation message sends "false" as a response to the invocation query')
+
+test.todo('confirmation message buttons are hidden after action is confirmed or aborted')
