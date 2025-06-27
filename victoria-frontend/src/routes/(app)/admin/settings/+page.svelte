1108c914227ca020c5081ef4a21d77b71d5181d3
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-27 15:02:15 +0200

    feat: annotate components /admin/settings

--- victoria-frontend/src/routes/(app)/admin/settings/+page.svelte
+++ victoria-frontend/src/routes/(app)/admin/settings/+page.svelte
@@ -3,10 +3,11 @@
     import { LinkOutline, CogSolid, BrainSolid, ChevronRightOutline, ChevronLeftOutline } from "flowbite-svelte-icons";
 </script>
 
-<!-- TODO: Decide how to abstract the general layout of these settings menus into a Component - the back button and such -->
+<!-- COMPONENT: SettingsDetailView -->
+<!-- COMPONENT: DetailView -->
 <div class="w-full h-full overflow-y-auto">
     <div class="hidden md:inline float-left">
-        <!-- This is a back button -->
+        <!-- COMPONENT: BackButton -->
         <div class="flex flex-row hover:bg-slate-500 rounded-md p-1">
             <ChevronLeftOutline class="h-8 w-8 my-auto"/>
             <div class="content-center mr-2">
@@ -17,7 +18,7 @@
 
     <div class="flex flex-col content-center mx-2 md:m-auto md:max-w-96">
         <div class="md:hidden">
-            <!-- This is a back button -->
+            <!-- COMPONENT: BackButton -->
             <div class="flex flex-row hover:bg-slate-500 rounded-md p-1">
                 <ChevronLeftOutline class="h-8 w-8 my-auto"/>
                 <div class="content-center mr-2">
@@ -27,11 +28,11 @@
         </div>            
 
         <div class="my-2">
-            <!-- This is a DetailViewSection -->
+            <!-- COMPONENT: DetailViewSection -->
             <Label class="sticky top-0 bg-white px-2 z-40 font-bold">Settings</Label>
             <div class="bg-slate-400 rounded-md p-2">
                 <div class="flex flex-col space-y-2">
-                    <!-- This is an OptionsRouteButton -->
+                    <!-- COMPONENT: OptionsRouteButton -->
                     <div class="flex flex-row w-full hover:bg-slate-500 p-1 rounded-md">
                         <div class="rounded-full bg-black p-1">
                             <CogSolid class="w-6 h-6 text-orange-500"/>
@@ -42,7 +43,7 @@
                         <ChevronRightOutline class="h-8 w-8 my-auto ml-auto"/>
                     </div>
 
-                    <!-- This is an OptionsRouteButton -->
+                    <!-- COMPONENT: OptionsRouteButton -->
                     <div class="flex flex-row w-full hover:bg-slate-500 p-1 rounded-md">
                         <div class="rounded-full bg-black p-1">
                             <LinkOutline class="w-6 h-6 text-purple-500"/>
@@ -53,7 +54,7 @@
                         <ChevronRightOutline class="h-8 w-8 my-auto ml-auto"/>
                     </div>
 
-                    <!-- This is an OptionsRouteButton -->
+                    <!-- COMPONENT: OptionsRouteButton -->
                     <div class="flex flex-row w-full hover:bg-slate-500 p-1 rounded-md">
                         <div class="rounded-full bg-black p-1">
                             <BrainSolid class="w-6 h-6 text-white"/>
