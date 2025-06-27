b368c0685d10147543255f1c39fe7cbaa1d53167
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-27 19:49:20 +0200

    feat: specs for buttons

---
+++ victoria-frontend/src/lib/components/buttons/OptionsRouteButton.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/svelte';
+
+test.todo('options route button routes to provided route on click', async () => {
+    assert.fail('not implemented')
+})
