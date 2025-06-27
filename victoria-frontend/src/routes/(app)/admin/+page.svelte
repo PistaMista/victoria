16865a29fda51ce70a70036355fa6a4373eee5f9
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-27 14:44:50 +0200

    feat: annotate components for /admin

--- victoria-frontend/src/routes/(app)/admin/+page.svelte
+++ victoria-frontend/src/routes/(app)/admin/+page.svelte
@@ -3,14 +3,16 @@
     import { HammerSolid, CogSolid, RocketSolid, UserSolid, ChevronRightOutline } from "flowbite-svelte-icons";
 </script>
 
+<!-- COMPONENT: AdminDetailView -->
+<!-- COMPONENT: DetailView -->
 <div class="w-full h-full overflow-y-auto">
     <div class="flex flex-col content-center mx-2 md:m-auto md:max-w-96">
         <div class="my-2">
-            <!-- This is a DetailViewSection -->
+            <!-- COMPONENT: DetailViewSection -->
             <Label class="sticky top-0 bg-white px-2 z-40 font-bold">Administration options</Label>
             <div class="bg-slate-400 rounded-md p-2">
                 <div class="flex flex-col space-y-2">
-                    <!-- This is an OptionsRouteButton -->
+                    <!-- COMPONENT: OptionsRouteButton -->
                     <div class="flex flex-row w-full hover:bg-slate-500 p-1 rounded-md">
                         <div class="rounded-full bg-black p-1">
                             <HammerSolid class="w-6 h-6 text-orange-500"/>
@@ -21,7 +23,7 @@
                         <ChevronRightOutline class="h-8 w-8 my-auto ml-auto"/>
                     </div>
 
-                    <!-- This is an OptionsRouteButton -->
+                    <!-- COMPONENT: OptionsRouteButton -->
                     <div class="flex flex-row w-full hover:bg-slate-500 p-1 rounded-md">
                         <div class="rounded-full bg-black p-1">
                             <RocketSolid class="w-6 h-6 text-purple-500"/>
@@ -32,7 +34,7 @@
                         <ChevronRightOutline class="h-8 w-8 my-auto ml-auto"/>
                     </div>
 
-                    <!-- This is an OptionsRouteButton -->
+                    <!-- COMPONENT: OptionsRouteButton -->
                     <div class="flex flex-row w-full hover:bg-slate-500 p-1 rounded-md">
                         <div class="rounded-full bg-black p-1">
                             <UserSolid class="w-6 h-6 text-white"/>
@@ -43,7 +45,7 @@
                         <ChevronRightOutline class="h-8 w-8 my-auto ml-auto"/>
                     </div>
 
-                    <!-- This is an OptionsRouteButton -->
+                    <!-- COMPONENT: OptionsRouteButton -->
                     <div class="flex flex-row w-full hover:bg-slate-500 p-1 rounded-md">
                         <div class="rounded-full bg-black p-1">
                             <CogSolid class="w-6 h-6 text-blue-500"/>
