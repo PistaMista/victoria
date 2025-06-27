3aab1789ef35d3fcc4897321f4345a7def4186af
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-27 13:05:58 +0200

    feat: annotate components for /chats

--- victoria-frontend/src/routes/(app)/chats/+page.svelte
+++ victoria-frontend/src/routes/(app)/chats/+page.svelte
@@ -4,10 +4,14 @@
     import { ChevronDoubleDownOutline, ChevronDownOutline, PlusOutline } from "flowbite-svelte-icons";
 </script>
 
+<!-- COMPONENT: ListView -->
 <div class="flex flex-col w-full h-full">
     <div class="p-1 bg-slate-400 flex flex-col md:flex-row">
+        <!-- COMPONENT: ListViewHeaderSearchBar -->
         <Search class="grow max-h-8 md:max-w-72 mb-1 md:mb-0"></Search>
+        
         <div class="mx-2 content-center flex flex-row">
+            <!-- COMPONENT: ListViewHeaderDropdown -->
             <div class="hover:bg-slate-300 p-1 rounded-md">
                 <Button class="flex flex-row">Sort<ChevronDownOutline class="w-6 h-6"/></Button>
                 <Dropdown>
@@ -16,6 +20,7 @@
                     <DropdownItem>Importance</DropdownItem>
                 </Dropdown>
             </div>
+            <!-- COMPONENT: ListViewHeaderDropdown -->
             <div class="hover:bg-slate-300 p-1 rounded-md">
                 <Button class="flex flex-row">Agent<ChevronDownOutline class="w-6 h-6"/></Button>
                 <Dropdown>
@@ -23,6 +28,7 @@
                     <DropdownItem>Researcher</DropdownItem>
                 </Dropdown>
             </div>
+            <!-- COMPONENT: ListViewHeaderButton -->
             <div class="hover:bg-slate-300 p-1 rounded-md">
                 <Button class="flex flex-row text-nowrap"><PlusOutline class="w-6 h-6"/></Button>
             </div>
@@ -30,6 +36,7 @@
     </div>
     
     <div class="flex flex-col grow min-h-0 bg-slate-300 overflow-y-scroll">
+        <!-- COMPONENT: ChatListItem (styling left to ListView) -->
         <div class="flex flex-col px-2 my-1 border-t-2 border-black hover:bg-slate-400">
             <div class="font-bold">System administration</div>
             <div>Discusses various system administration activities.</div>
