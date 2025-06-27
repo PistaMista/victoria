9896f90cff5d7ab28a3021281fffbe342e21a652
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-27 15:12:33 +0200

    feat: annotate components for /admin/settings/connections/add

--- victoria-frontend/src/routes/(app)/admin/settings/connections/add/+page.svelte
+++ victoria-frontend/src/routes/(app)/admin/settings/connections/add/+page.svelte
@@ -3,10 +3,11 @@
     import {  ChevronLeftOutline, PlusOutline, TrashBinSolid, CogSolid } from "flowbite-svelte-icons";
 </script>
 
-<!-- TODO: Decide how to abstract the general layout of these settings menus into a Component - the back button and such -->
+<!-- COMPONENT: ConnectionDetailView -->
+<!-- COMPONENT: DetailView -->
 <div class="w-full h-full overflow-y-auto">
     <div class="hidden md:inline float-left">
-        <!-- This is a back button -->
+        <!-- COMPONENT: BackButton -->
         <div class="flex flex-row hover:bg-slate-500 rounded-md p-1">
             <ChevronLeftOutline class="h-8 w-8 my-auto"/>
             <div class="content-center mr-2">
@@ -17,7 +18,7 @@
 
     <div class="flex flex-col content-center mx-2 md:m-auto md:max-w-96">
         <div class="md:hidden">
-            <!-- This is a back button -->
+            <!-- COMPONENT: BackButton -->
             <div class="flex flex-row hover:bg-slate-500 rounded-md p-1">
                 <ChevronLeftOutline class="h-8 w-8 my-auto"/>
                 <div class="content-center mr-2">
@@ -27,7 +28,7 @@
         </div>            
 
         <div class="my-2">
-            <!-- This is a DetailViewSection -->
+            <!-- COMPONENT: DetailViewSection -->
             <Label class="sticky top-0 bg-white px-2 z-40 font-bold">URL</Label>
             <div class="bg-slate-400 rounded-md p-2">
                 <Input/>
@@ -35,13 +36,14 @@
         </div>
 
         <div class="my-2">
-            <!-- This is a DetailViewSection -->
+            <!-- COMPONENT: DetailViewSection -->
             <Label class="sticky top-0 bg-white px-2 z-40 font-bold">API Key</Label>
             <div class="bg-slate-400 rounded-md p-2">
                 <Input/>
             </div>
         </div>
         
+        <!-- COMPONENT: SaveButton -->
         <Button class="font-bold bg-lime-500">
             ADD CONNECTION
         </Button>
