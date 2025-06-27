09caa24cc2f7dd2097211fda6bea74e3f75fe4c3
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-27 20:04:07 +0200

    feat: specs for UnreadMessageMiniCard

---
+++ victoria-frontend/src/lib/components/cards/AgentCard.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "vitest";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/svelte';
+
+test.todo('agent card shows name of agent with given id', async () => {
+})
+
+test.todo('agent card shows status of agent with given id', async () => {
+})
+
+test.todo('agent card shows monologue names of agent with given id', async () => {
+})
+
+test.todo('agent card routes to agent editing when context button is pressed', async () => {
+})
---
+++ victoria-frontend/src/lib/components/cards/MonologueMiniCard.spec.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "vitest";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/svelte';
+
+test.todo('monologue mini card shows name of monologue with given id', async () => {
+})
+
+test.todo('monologue mini card shows status of monologue with given id', async () => {
+})
+
+test.todo('monologue mini card routes to monologue detail when clicked', async () => {
+})
