3bd77f721060df2d8a9953d679fdccae255c8135
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-27 12:58:03 +0200

    feat: outline dashboard components

--- victoria-frontend/src/routes/(app)/+page.svelte
+++ victoria-frontend/src/routes/(app)/+page.svelte
@@ -6,11 +6,13 @@
 
 <div class="w-full h-full overflow-y-auto">
     <div class="flex flex-col md:flex-row md:flex-wrap justify-center p-2">
+        <!-- COMPONENT: DashboardSection -->
         <div class="flex flex-col m-1">
             <div class="flex justify-center text-xl font-bold">
                 Monologue Activity
             </div>
             <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
+                <!-- COMPONENT: MonologueMiniCard-->
                 <div class="flex bg-slate-400 p-1 rounded-md">
                     Check system dashboard
                     <div class="grow mx-2"/>
@@ -29,28 +31,12 @@
             </div>
         </div>
 
-        <div class="flex flex-col m-1">
-            <div class="flex justify-center text-xl font-bold">
-                Pending Tasks
-            </div>
-            <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
-                <div class="flex bg-slate-400 p-1 rounded-md">
-                    Collect news
-                    <div class="grow mx-2"/>
-                    <ExclamationCircleOutline/>
-                </div>
-                <div class="flex bg-slate-400 p-1 rounded-md">
-                    Research thesis ideas
-                    <div class="grow mx-2"/>
-                    <CheckCircleOutline/>
-                </div>
-            </div>
-        </div>
-
+        <!-- COMPONENT: DashboardSection -->
         <div class="flex flex-col m-1">
             <div class="flex justify-center text-xl font-bold">
                 Agent Status
             </div>
+            <!-- COMPONENT: AgentMiniCard -->
             <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
                 <div class="flex bg-slate-400 p-1 rounded-md">
                     Researcher
@@ -64,66 +50,18 @@
                 </div>
             </div>
         </div>
+
+        <!-- COMPONENT: DashboardSection -->
         <div class="flex flex-col m-1">
             <div class="flex justify-center text-xl font-bold">
                 Unread Messages
             </div>
             <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
+                <!-- COMPONENT: UnreadMessageMiniCard -->
                 <div class="flex bg-slate-400 p-1 rounded-md">
                     Failed system units
                     <div class="grow mx-2"/>
                     <EnvelopeOutline/>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-col m-1">
-            <div class="flex justify-center text-xl font-bold">
-                Other Widget
-            </div>
-            <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
-                <div class="flex bg-slate-400 p-1 rounded-md">
-                    Researcher
-                    <div class="grow mx-2"/>
-                    <CheckOutline/>
-                </div>
-                <div class="flex bg-slate-400 p-1 rounded-md">
-                    Sysadmin
-                    <div class="grow mx-2"/>
-                    <ExclamationCircleOutline/>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-col m-1">
-            <div class="flex justify-center text-xl font-bold">
-                Other Widget
-            </div>
-            <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
-                <div class="flex bg-slate-400 p-1 rounded-md">
-                    Researcher
-                    <div class="grow mx-2"/>
-                    <CheckOutline/>
-                </div>
-                <div class="flex bg-slate-400 p-1 rounded-md">
-                    Sysadmin
-                    <div class="grow mx-2"/>
-                    <ExclamationCircleOutline/>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-col m-1">
-            <div class="flex justify-center text-xl font-bold">
-                Other Widget
-            </div>
-            <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
-                <div class="flex bg-slate-400 p-1 rounded-md">
-                    asdasdasd
-                    <div class="grow mx-2"/>
-                    <CheckOutline/>
-                </div>
-                <div class="flex bg-slate-400 p-1 rounded-md">
-                    Sysadmin
-                    <div class="grow mx-2"/>
-                    <ExclamationCircleOutline/>
                 </div>
             </div>
         </div>
