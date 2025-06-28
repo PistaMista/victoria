13a78d192cc3e40aba16bf3a46cbe76c9a7b7bfd
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-28 18:06:37 +0200

    feat: specs for triggers

---
+++ victoria-frontend/src/lib/components/toggles/TriggerToggle.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/svelte';
+
+test.todo('trigger toggle shows name of trigger with given id')
+
+test.todo('clicking trigger toggle of disabled trigger sends enable request')
+
+test.todo('clicking trigger toggle of enabled trigger sends disable request')
