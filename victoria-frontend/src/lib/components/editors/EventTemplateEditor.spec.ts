7e2742a97a2eef2a1435f06724151abc42680e32
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-28 16:14:01 +0200

    feat: specs for event template editor

---
+++ victoria-frontend/src/lib/components/editors/EventTemplateEditor.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/svelte';
+
+test.todo('event template editor displays parser variables')
+
+test.todo('event template editor displays inline error on invalid syntax')
+
+test.todo('event template editor displays inline error when using undefined variable')
+
+test.todo('event template editor highlights error in the text')
+
+test.todo('event template editor allows editing template')
