db25f149834b010e52d3f5f020f666e042fb82b6
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-28 18:29:33 +0200

    feat: add trivial tests to make vitest not fail on empty test suite

---
+++ victoria-frontend/src/lib/components/chat/messages/ImageContent.spec.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/svelte';
+
+test.todo('image message content contains image element')
--- victoria-frontend/src/lib/components/monologue/thoughts/SuccessContent.spec.ts
+++ victoria-frontend/src/lib/components/monologue/thoughts/SuccessContent.spec.ts
@@ -1,3 +1,5 @@
 import { expect, test } from "vitest";
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/svelte';
+
+test.todo('success thought content contains "success" string')
