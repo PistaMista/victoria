2002334750a37d550e7ae781dbdcc72adee45aca
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-28 18:00:49 +0200

    feat: specs for sections

---
+++ victoria-frontend/src/lib/components/sections/DashboardSection.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/svelte';
+
+test.todo('dashboard section shows section title')
+
+test.todo('dashboard section contains provided child components')
---
+++ victoria-frontend/src/lib/components/sections/LabeledSetting.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/svelte';
+
+test.todo('labeled setting shows setting label')
+
+test.todo('labeled setting displays given child component')
