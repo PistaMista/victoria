70b0776857c5849fcef5df5344a067502344ee67
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-28 16:30:02 +0200

    feat: specs for indicators

---
+++ victoria-frontend/src/lib/components/indicators/MonologueStatusIndicator.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "vitest";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/svelte';
+
+test.todo('monologue status indicator displays status of monologue with given id')
+
+test.todo('monologue status indicator can optionally display runtime of monologue with given id')
+
+test.todo('monologue status indicator can optionally display launch date of monologue with given id')
+
+test.todo('monologue status indicator can optionally display finish date of monologue with given id')
