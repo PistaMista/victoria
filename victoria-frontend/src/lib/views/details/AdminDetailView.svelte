178ddfd4a44ad4e9086c76e75899bcb56f762fc2
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-29 21:04:25 +0200

    feat: factor out AdminDetailView

---
+++ victoria-frontend/src/lib/views/details/AdminDetailView.svelte
@@ -0,0 +1,15 @@
+<script>
+    import OptionsRouteButton from "$lib/components/buttons/OptionsRouteButton.svelte";
+    import DetailViewSection from "$lib/components/sections/DetailViewSection.svelte";
+    import DetailView from "$lib/views/DetailView.svelte";
+    import { HammerSolid, CogSolid, RocketSolid, UserSolid } from "flowbite-svelte-icons";
+</script>
+
+<DetailView>
+    <DetailViewSection title="Administration options">
+        <OptionsRouteButton icon={HammerSolid} text="Actions"/>
+        <OptionsRouteButton icon={RocketSolid} text="Triggers"/>
+        <OptionsRouteButton icon={UserSolid} text="Users"/>
+        <OptionsRouteButton icon={CogSolid} text="Settings"/>
+    </DetailViewSection>
+</DetailView>
