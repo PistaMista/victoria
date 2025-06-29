48e6067c8811c8d68190b44c60e2636a3128e130
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-29 19:54:14 +0200

    feat: factor out ConnectionDetailView

--- victoria-frontend/src/routes/(app)/admin/settings/connections/add/+page.svelte
+++ victoria-frontend/src/routes/(app)/admin/settings/connections/add/+page.svelte
@@ -1,51 +1,5 @@
 <script>
-    import { Label, Button, Input } from "flowbite-svelte";
-    import {  ChevronLeftOutline, PlusOutline, TrashBinSolid, CogSolid } from "flowbite-svelte-icons";
+    import ConnectionDetailView from "$lib/views/details/ConnectionDetailView.svelte";
 </script>
 
-<!-- COMPONENT: ConnectionDetailView -->
-<!-- COMPONENT: DetailView -->
-<div class="w-full h-full overflow-y-auto">
-    <div class="hidden md:inline float-left">
-        <!-- COMPONENT: BackButton -->
-        <div class="flex flex-row hover:bg-slate-500 rounded-md p-1">
-            <ChevronLeftOutline class="h-8 w-8 my-auto"/>
-            <div class="content-center mr-2">
-                Back
-            </div>
-        </div>
-    </div>
-
-    <div class="flex flex-col content-center mx-2 md:m-auto md:max-w-96">
-        <div class="md:hidden">
-            <!-- COMPONENT: BackButton -->
-            <div class="flex flex-row hover:bg-slate-500 rounded-md p-1">
-                <ChevronLeftOutline class="h-8 w-8 my-auto"/>
-                <div class="content-center mr-2">
-                    Back
-                </div>
-            </div>
-        </div>            
-
-        <div class="my-2">
-            <!-- COMPONENT: DetailViewSection -->
-            <Label class="sticky top-0 bg-white px-2 z-40 font-bold">URL</Label>
-            <div class="bg-slate-400 rounded-md p-2">
-                <Input/>
-            </div>
-        </div>
-
-        <div class="my-2">
-            <!-- COMPONENT: DetailViewSection -->
-            <Label class="sticky top-0 bg-white px-2 z-40 font-bold">API Key</Label>
-            <div class="bg-slate-400 rounded-md p-2">
-                <Input/>
-            </div>
-        </div>
-        
-        <!-- COMPONENT: SaveButton -->
-        <Button class="font-bold bg-lime-500">
-            ADD CONNECTION
-        </Button>
-    </div>
-</div>
+<ConnectionDetailView/>
