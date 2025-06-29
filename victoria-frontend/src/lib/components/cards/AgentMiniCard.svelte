50470f97bb74081bb74401ee8df4cc1d3c6a96a9
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-29 21:10:54 +0200

    feat: split dashboard into components

---
+++ victoria-frontend/src/lib/components/cards/AgentMiniCard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { CheckOutline } from "flowbite-svelte-icons";
+</script>
+
+<div class="flex bg-slate-400 p-1 rounded-md">
+    Researcher
+    <div class="grow mx-2"/>
+    <CheckOutline/>
+</div>
---
+++ victoria-frontend/src/lib/components/sections/DashboardSection.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let title: String;
+</script>
+<div class="flex flex-col m-1">
+    <div class="flex justify-center text-xl font-bold">
+        {title}
+    </div>
+    <div class="bg-slate-300 rounded-md flex flex-col p-2 space-y-2">
+        <slot/>
+    </div>
+</div>
