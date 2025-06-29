05d73c0e53c1532d3e3f8285b2d0710395461977
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-29 20:41:46 +0200

    feat: factor out TriggerDetailView

---
+++ victoria-frontend/src/lib/components/dropdowns/GeneralDropdown.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
+    import { ChevronDownOutline } from "flowbite-svelte-icons";
+</script>
+
+<div>
+    <Button class="flex flex-row w-full">IDENTITY<ChevronDownOutline class="w-6 h-6"/></Button>
+    <Dropdown>
+        <DropdownItem>Identity</DropdownItem>
+        <DropdownItem>JSON</DropdownItem>
+        <DropdownItem>XML</DropdownItem>
+    </Dropdown>
+</div>
---
+++ victoria-frontend/src/lib/views/details/TriggerDetailView.svelte
@@ -0,0 +1,57 @@
+<script>
+    import CreateButton from "$lib/components/buttons/CreateButton.svelte";
+    import DeleteButton from "$lib/components/buttons/DeleteButton.svelte";
+    import SaveButton from "$lib/components/buttons/SaveButton.svelte";
+    import GeneralDropdown from "$lib/components/dropdowns/GeneralDropdown.svelte";
+    import EventTemplateEditor from "$lib/components/editors/EventTemplateEditor.svelte";
+    import DetailViewSection from "$lib/components/sections/DetailViewSection.svelte";
+    import LabeledSetting from "$lib/components/sections/LabeledSetting.svelte";
+    import ChatTriggerSettings from "$lib/components/triggers/ChatTriggerSettings.svelte";
+    import PollTriggerSettings from "$lib/components/triggers/PollTriggerSettings.svelte";
+    import TimerTriggerSettings from "$lib/components/triggers/TimerTriggerSettings.svelte";
+    import DetailView from "$lib/views/DetailView.svelte";
+    import { Input } from "flowbite-svelte";
+</script>
+
+<DetailView backRoute="/admin/triggers">
+    <DetailViewSection title="Trigger">
+        <LabeledSetting label="Name">
+            <Input/>
+        </LabeledSetting>
+
+        <LabeledSetting label="Type">
+            <!-- Type: TIMER, CHAT, WEBHOOK, POLL-->
+            <GeneralDropdown/>
+        </LabeledSetting>
+        
+        <PollTriggerSettings/>
+        <ChatTriggerSettings/>
+        <TimerTriggerSettings/>
+    </DetailViewSection>
+    
+    <DetailViewSection title="Parser">
+        <LabeledSetting label="Type">
+            <GeneralDropdown/>
+        </LabeledSetting>
+
+        <LabeledSetting label="Settings">
+            SOME SETTINGS TO CHANGE
+        </LabeledSetting>
+    </DetailViewSection>
+    
+    <DetailViewSection title="Template">
+        <EventTemplateEditor/>
+    </DetailViewSection>
+    
+    <DetailViewSection title="Delete trigger">
+        <DeleteButton/>
+    </DetailViewSection>
+    
+    <DetailViewSection title="Save trigger">
+        <SaveButton/>
+    </DetailViewSection>
+    
+    <DetailViewSection title="Create trigger">
+        <CreateButton/>
+    </DetailViewSection>
+</DetailView>
