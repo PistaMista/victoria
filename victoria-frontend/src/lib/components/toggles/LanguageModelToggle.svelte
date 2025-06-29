27e3407f819fb2a61184ac8b8710ef1d167184ef
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-29 20:01:14 +0200

    feat: factor out ModelSettingsDetailView

---
+++ victoria-frontend/src/lib/components/toggles/LanguageModelToggle.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Checkbox } from "flowbite-svelte";
+</script>
+
+<div class="flex flex-row bg-slate-500 rounded-md p-1">
+    <div class="text-white mr-auto">
+        gemma3:12b
+    </div>
+    <Checkbox/>
+</div>
--- victoria-frontend/src/routes/(app)/admin/settings/models/+page.svelte
+++ victoria-frontend/src/routes/(app)/admin/settings/models/+page.svelte
@@ -1,44 +1,5 @@
 <script>
-    import { Label, Button, Input, Checkbox } from "flowbite-svelte";
-    import {  ChevronLeftOutline, PlusOutline, TrashBinSolid, CogSolid } from "flowbite-svelte-icons";
+    import ModelSettingsDetailView from "$lib/views/details/ModelSettingsDetailView.svelte";
 </script>
 
-<!-- COMPONENT: ModelSettingsDetailView -->
-<!-- COMPONENT: DetailView -->
-<div class="w-full h-full overflow-y-auto">
-    <div class="hidden md:inline float-left">
-        <!-- COMPONENT: BackButton -->
-        <div class="flex flex-row hover:bg-slate-500 rounded-md p-1">
-            <ChevronLeftOutline class="h-8 w-8 my-auto"/>
-            <div class="content-center mr-2">
-                Back
-            </div>
-        </div>
-    </div>
-
-    <div class="flex flex-col content-center mx-2 md:m-auto md:max-w-96">
-        <div class="md:hidden">
-            <!-- COMPONENT: BackButton -->
-            <div class="flex flex-row hover:bg-slate-500 rounded-md p-1">
-                <ChevronLeftOutline class="h-8 w-8 my-auto"/>
-                <div class="content-center mr-2">
-                    Back
-                </div>
-            </div>
-        </div>            
-
-        <div class="my-2">
-            <!-- COMPONENT: DetailViewSection -->
-            <Label class="sticky top-0 bg-white px-2 z-40 font-bold">Ollama models</Label>
-            <div class="bg-slate-400 rounded-md p-2">
-                <!-- COMPONENT: LanguageModelToggle -->
-                <div class="flex flex-row bg-slate-500 rounded-md p-1">
-                    <div class="text-white mr-auto">
-                        gemma3:12b
-                    </div>
-                    <Checkbox/>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<ModelSettingsDetailView/>
