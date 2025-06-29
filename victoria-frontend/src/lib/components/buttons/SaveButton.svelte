a8c3704ade544bf7309d45f11677429af720457d
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-29 18:10:10 +0200

    feat: factor out ChatOptionsView

---
+++ victoria-frontend/src/lib/components/buttons/SaveButton.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { Button } from "flowbite-svelte";
+    import { FloppyDiskSolid } from "flowbite-svelte-icons";
+</script>
+
+<Button class="w-full font-bold bg-lime-600"><FloppyDiskSolid/>SAVE</Button>
---
+++ victoria-frontend/src/lib/components/toggles/ActionToggle.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Checkbox } from "flowbite-svelte";
+</script>
+
+<div class="flex flex-row m-1 bg-slate-200 rounded-md p-1">
+    <div class="font-bold grow">
+        Send chat message
+    </div>
+    <Checkbox/>
+</div>
