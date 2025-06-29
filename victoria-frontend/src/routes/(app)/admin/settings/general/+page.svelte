57dae9da59e8f20720233eeb35990b84fcbd245a
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-29 19:58:57 +0200

    feat: factor out GeneralSettingsDetailView

--- victoria-frontend/src/routes/(app)/admin/settings/general/+page.svelte
+++ victoria-frontend/src/routes/(app)/admin/settings/general/+page.svelte
@@ -1,31 +1,5 @@
 <script>
-    import { Label, Button, Input } from "flowbite-svelte";
-    import {  ChevronLeftOutline, PlusOutline, TrashBinSolid, CogSolid } from "flowbite-svelte-icons";
+    import GeneralSettingsDetailView from "$lib/views/details/GeneralSettingsDetailView.svelte";
 </script>
 
-<!-- COMPONENT: GeneralSettingsDetailView -->
-<!-- COMPONENT: DetailView -->
-<div class="w-full h-full overflow-y-auto">
-    <div class="hidden md:inline float-left">
-        <!-- COMPONENT: BackButton -->
-        <div class="flex flex-row hover:bg-slate-500 rounded-md p-1">
-            <ChevronLeftOutline class="h-8 w-8 my-auto"/>
-            <div class="content-center mr-2">
-                Back
-            </div>
-        </div>
-    </div>
-
-    <div class="flex flex-col content-center mx-2 md:m-auto md:max-w-96">
-        <div class="md:hidden">
-            <!-- COMPONENT: BackButton -->
-            <div class="flex flex-row hover:bg-slate-500 rounded-md p-1">
-                <ChevronLeftOutline class="h-8 w-8 my-auto"/>
-                <div class="content-center mr-2">
-                    Back
-                </div>
-            </div>
-        </div>            
-
-    </div>
-</div>
+<GeneralSettingsDetailView/>
