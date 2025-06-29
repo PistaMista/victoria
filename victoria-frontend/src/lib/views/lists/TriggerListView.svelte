09642e197dc570fdab3b9891290705a070fc7d7c
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-29 20:46:55 +0200

    feat: factor out TriggerListView

---
+++ victoria-frontend/src/lib/views/lists/TriggerListView.svelte
@@ -0,0 +1,16 @@
+<script>
+    import ListViewHeaderButton from "$lib/components/buttons/ListViewHeaderButton.svelte";
+    import TriggerListItem from "$lib/components/lists/items/TriggerListItem.svelte";
+    import ListViewHeaderSearchBar from "$lib/components/search/ListViewHeaderSearchBar.svelte";
+    import ListView from "$lib/views/ListView.svelte";
+    import { PlusOutline } from "flowbite-svelte-icons";
+</script>
+
+<ListView backRoute="/admin">
+    <ListViewHeaderSearchBar slot="header"/>
+    <ListViewHeaderButton slot="header"><PlusOutline class="w-6 h-8 my-auto"/></ListViewHeaderButton>
+    
+    <TriggerListItem slot="items"/>
+    <TriggerListItem slot="items"/>
+    <TriggerListItem slot="items"/>
+</ListView>
