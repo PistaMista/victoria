cff60319c5493ebb559052006269c1aefe5ef291
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-06-29 19:22:32 +0200

    feat: change color of save button

--- victoria-frontend/src/lib/components/buttons/SaveButton.svelte
+++ victoria-frontend/src/lib/components/buttons/SaveButton.svelte
@@ -3,4 +3,4 @@
     import { FloppyDiskSolid } from "flowbite-svelte-icons";
 </script>
 
-<Button class="w-full font-bold bg-lime-600"><FloppyDiskSolid/>SAVE</Button>
+<Button class="w-full font-bold bg-blue-600"><FloppyDiskSolid/>SAVE</Button>
