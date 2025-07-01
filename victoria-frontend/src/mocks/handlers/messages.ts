b6ff73de45a2637597a0a7d9d6f9edc5bb80d520
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-01 13:45:12 +0200

    feat: mock /api/messages/:id

--- victoria-frontend/src/mocks/handlers/messages.ts
+++ victoria-frontend/src/mocks/handlers/messages.ts
@@ -15,21 +15,33 @@ export const handlers = [
                 return HttpResponse.json(
                     {
                         id: 2,
-                        type: 'image'
+                        type: 'image',
+                        content: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAVCAIAAADelck2AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAVZJREFUeNrUlM9OwkAQxme229Y/gMWkhCsGmngQEvUd4GZ4O9/AAzyB0fgGcFRClBOikpRqG2m73V2XJ9ga24PfefeXmW++GZQ3V5B8QeGyaxS2PrDv4tEiI0AolCFCCZSmEtEUBFe+lOA1p7DvADH0Tw0ClgEIkHDgIkdCqihjlTyp6w2jIJlO3wWX3a5bbxxCpqUjSim1FSxX2/F41modIeJiEQwGJ167VswY726fe2fHq+Xn7PHj8tx9uH/hXOYYo06KwphQ9Y5GT1HElCGWZYRh6jj2X9GGgaapxgfDoRdFaaViJUlWrZraj7m8fnvded3p1BFhPg/6/Vbb03uNm9jX0iklvh9PJiohotdrNJsVlnFdPhAvrk+DOMhji2Xv4p+mnGf6Rp09h6636406fnkU/mIZmWTUVEMiZuF7rrD/8/IRJlgZXIWl7oFbhtcqIT8CDAAeYYokfXCFwwAAAABJRU5ErkJggg=='
                     }
                 )
             case '3':
                 return HttpResponse.json(
                     {
                         id: 2,
-                        type: 'choice-prompt'
+                        type: 'choice-prompt',
+                        content: {
+                            queryId: 1,
+                            choices: [
+                                { value: 2 },
+                                { value: "this is a value" },
+                                { value: true }
+                            ]
+                        }
                     }
                 )
             case '4':
                 return HttpResponse.json(
                     {
                         id: 2,
-                        type: 'action-confirmation'
+                        type: 'action-confirmation',
+                        content: {
+                            queryId: 20
+                        }
                     }
                 )
         }
