b3236e9ce7f998fec6981ae5e0b334c67e3e25b2
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-01 19:41:05 +0200

    feat: types for event

---
+++ victoria-frontend/src/lib/types/event.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const Event = z.object({
+    triggerId: z.number(),
+    content: z.string(),
+});
+export type Event = z.infer<typeof Event>;
--- victoria-frontend/src/mocks/handlers/events.ts
+++ victoria-frontend/src/mocks/handlers/events.ts
@@ -1,8 +1,9 @@
 import { http, HttpResponse } from "msw"
+import type { Event } from "$lib/types/event"
 
 export const handlers = [
     http.get('/api/events/:id', () => {
-        return HttpResponse.json({
+        return HttpResponse.json<Event>({
             triggerId: 1,
             content: "An email has arrived: 'Join the 2025 game access conference...'"
         })
