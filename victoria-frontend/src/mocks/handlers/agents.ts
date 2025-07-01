6cb0ccfcd57df8fa643d687749ad0cb999e0be8b
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-01 19:28:56 +0200

    feat: types for agent

--- victoria-frontend/src/mocks/handlers/agents.ts
+++ victoria-frontend/src/mocks/handlers/agents.ts
@@ -1,16 +1,20 @@
 import { http, HttpResponse } from "msw"
+import type { Agent, AgentListItem } from "$lib/types/agent";
+import { MonologueListItem } from "$lib/types/monologue";
 
 export const handlers = [
     http.get('/api/agents', () => {
-        return HttpResponse.json(
+        return HttpResponse.json<Array<AgentListItem>>(
             [
                 {
                     id: 1,
-                    name: 'Cook'
+                    name: 'Cook',
+                    status: 'BUSY'
                 },
                 {
                     id: 2,
-                    name: 'Researcher'
+                    name: 'Researcher',
+                    status: 'IDLE'
                 }
             ]
         )
@@ -20,18 +24,20 @@ export const handlers = [
             name: string
         };
 
-        return HttpResponse.json(
+        return HttpResponse.json<AgentListItem>(
             {
                 id: 3,
                 name: body.name,
+                status: 'IDLE'
             }
         )
     }),
     
     http.get('/api/agents/:id', ({params: { id }}) => {
-        return HttpResponse.json({
+        return HttpResponse.json<Agent>({
             id: Number(id),
             name: "Cook",
+            status: 'IDLE',
             baseModelId: 1,
             systemPrompt: "You're a Cook that generates recipes for the week...",
             modelParameters: {
@@ -43,24 +49,26 @@ export const handlers = [
         })
     }),
     http.put('/api/agents/:id', () => {
-        return HttpResponse.json(true)
+        return HttpResponse.json<Boolean>(true)
     }),
     http.delete('/api/agents/:id', () => {
-        return HttpResponse.json(true)
+        return HttpResponse.json<Boolean>(true)
     }),
 
     http.get('/api/agents/:id/monologues', () => {
-        return HttpResponse.json(
+        return HttpResponse.json<Array<MonologueListItem>>(
             [
                 {
                     id: 1,
                     title: "Research thesis ideas",
+                    summary: "Searching web for sources",
                     status: "RUNNING"
                 },
                 {
                     id: 2,
                     title: "Respond to user message",
-                    status: "COMPLETED"
+                    summary: "Done",
+                    status: "SUCCESS"
                 }
             ]
         )
