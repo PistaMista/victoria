5ac68bc47256a1a1dbb7ade9b59b41c0efc402df
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-01 15:43:20 +0200

    feat: mock /api/events

--- victoria-frontend/src/mocks/handlers/events.ts
+++ victoria-frontend/src/mocks/handlers/events.ts
@@ -1,5 +1,10 @@
 import { http, HttpResponse } from "msw"
 
 export const handlers = [
-    http.get('/api/events/:id', () => {}),
+    http.get('/api/events/:id', () => {
+        return HttpResponse.json({
+            triggerId: 1,
+            content: "An email has arrived: 'Join the 2025 game access conference...'"
+        })
+    }),
 ]
