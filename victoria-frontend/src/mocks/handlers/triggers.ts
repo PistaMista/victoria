d71261426b9391c67e08870fcbfd8cef9dbf2cf9
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-01 21:50:44 +0200

    feat: types for trigger

--- victoria-frontend/src/mocks/handlers/triggers.ts
+++ victoria-frontend/src/mocks/handlers/triggers.ts
@@ -1,8 +1,9 @@
 import { http, HttpResponse } from "msw"
+import { TriggerListItem, Trigger } from "$lib/types/trigger"
 
 export const handlers = [
     http.get('/api/triggers', () => {
-        return HttpResponse.json([
+        return HttpResponse.json<Array<TriggerListItem>>([
             {
                 id: 1,
                 name: 'Generate recipes',
@@ -26,30 +27,32 @@ export const handlers = [
         ])
     }),
     http.post('/api/triggers', () => {
-        return HttpResponse.json({
-            id: 5
+        return HttpResponse.json<TriggerListItem>({
+            id: 5,
+            name: 'A new trigger',
+            type: 'timer'
         })
     }),
 
     http.get('/api/triggers/:id', ({ params: { id }}) => {
         switch (id) {
             case '1':
-                return HttpResponse.json({
+                return HttpResponse.json<Trigger>({
                     id: 1,
                     name: 'Generate recipes',
-                    type: 'timer',
                     settings: {
+                        type: 'timer',
                         interval: "2d"
                     },
                     parser: "identity",
                     template: "Based on the feed from the fridge camera, generate recipes for the week"
                 })
             case '2':
-                return HttpResponse.json({
+                return HttpResponse.json<Trigger>({
                     id: 2,
                     name: 'Retrieve news',
-                    type: 'poll',
                     settings: {
+                        type: 'poll',
                         interval: "1h",
                         url: "https://bbc.co.uk/rss"
                     },
@@ -57,22 +60,22 @@ export const handlers = [
                     template: "Based on the following news from BBC, notify the user of anything interesting: ${content}"                    
                 })
             case '3':
-                return HttpResponse.json({
+                return HttpResponse.json<Trigger>({
                     id: 3,
                     name: 'General chat',
-                    type: 'poll',
                     settings: {
+                        type: 'chat',
                         receiver: "general"
                     },
                     parser: "identity",
                     template: "A new chat message has arrived from the user: ${content}"                    
                 })
             case '4':
-                return HttpResponse.json({
+                return HttpResponse.json<Trigger>({
                     id: 4,
                     name: 'Discord chat message received',
-                    type: 'webhook',
                     settings: {
+                        type: 'webhook',
                         url: "/hook"
                     },
                     parser: "identity",
@@ -81,9 +84,9 @@ export const handlers = [
         }
     }),
     http.put('/api/triggers/:id', () => {
-        return HttpResponse.json(true)
+        return HttpResponse.json<Boolean>(true)
     }),
     http.delete('/api/triggers/:id', () => {
-        return HttpResponse.json(true)
+        return HttpResponse.json<Boolean>(true)
     }),
 ]
