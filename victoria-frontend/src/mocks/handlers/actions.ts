23675c12e0a67503d393c0141d4907e0411370b8
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-01 16:13:59 +0200

    feat: mock /api/actions

--- victoria-frontend/src/mocks/handlers/actions.ts
+++ victoria-frontend/src/mocks/handlers/actions.ts
@@ -1,5 +1,26 @@
 import { http, HttpResponse } from "msw"
 
 export const handlers = [
-    http.get('/api/actions', () => {}),
+    http.get('/api/actions', () => {
+        return HttpResponse.json([
+            {
+                id: 1,
+                repoId: 1,
+                name: "add_to_calendar",
+                displayName: "Add to calendar"
+            },
+            {
+                id: 2,
+                repoId: null,
+                name: "add_thought",
+                displayName: "Think"
+            },
+            {
+                id: 3,
+                repoId: null,
+                name: "start_monologue",
+                displayName: "Start monologue"
+            }
+        ])
+    }),
 ]
