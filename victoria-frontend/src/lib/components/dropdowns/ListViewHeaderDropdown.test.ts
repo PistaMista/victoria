76b223c9e72dd2f031ea28bcedef314e7877e858
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-03 00:49:19 +0200

    feat: implement ListViewHeaderDropdown

--- victoria-frontend/src/lib/components/dropdowns/ListViewHeaderDropdown.test.ts
+++ victoria-frontend/src/lib/components/dropdowns/ListViewHeaderDropdown.test.ts
@@ -1,7 +1,41 @@
 import { expect, test } from "vitest";
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/svelte';
+import { render, screen, waitFor } from '@testing-library/svelte';
+import ListViewHeaderDropdown, { type Option } from "./ListViewHeaderDropdown.svelte";
 
-test.todo('list view header dropdown shows set options when clicked')
+test('list view dropdown does not show options by default', async () => {
+    let { container } = render(ListViewHeaderDropdown, {
+        title: "Houses",
+        options: [
+            {displayName: "Thatched", ariaLabel: "Thatched", value: 1},
+            {displayName: "Shotgun", ariaLabel: "Shotgun", value: 2},
+            {displayName: "Tall", ariaLabel: "Tall", value: true}
+        ]
+    });
+    
+    expect(container).not.toHaveTextContent("Thatched");
+    expect(container).not.toHaveTextContent("Shotgun");
+    expect(container).not.toHaveTextContent("Tall");
+})
 
-test.todo('list view header dropdown sets option variable when option clicked')
+test('list view header dropdown shows set options when clicked', async () => {
+    const user = userEvent.setup();
+    const { getByRole, container } = render(ListViewHeaderDropdown, {
+        title: "Houses",
+        "aria-label": "Houses",
+        options: [
+            {displayName: "Thatched", ariaLabel: "Thatched", value: 1},
+            {displayName: "Shotgun", ariaLabel: "Shotgun", value: 2},
+            {displayName: "Tall", ariaLabel: "Tall", value: true}
+        ]
+    });
+    
+    const button = getByRole('button');
+    user.click(button);
+    
+    await waitFor(() => {
+        expect(container).toHaveTextContent("Thatched");
+        expect(container).toHaveTextContent("Shotgun");
+        expect(container).toHaveTextContent("Tall");
+    })
+})
