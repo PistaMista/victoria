0bf5def20f77052522e2aa225443f0d2bbdce273
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-02 21:18:49 +0200

    feat: implement chat list item

--- victoria-frontend/src/lib/components/lists/items/ChatListItem.svelte
+++ victoria-frontend/src/lib/components/lists/items/ChatListItem.svelte
@@ -1,4 +1,14 @@
-<div class="flex flex-col px-2 my-1 border-t-2 border-black hover:bg-slate-400">
-    <div class="font-bold">System administration</div>
-    <div>Discusses various system administration activities.</div>
-</div>
+<script lang="ts">
+    import type { Chat } from "$lib/types/chat";
+    import { goto } from "$app/navigation";
+    export let chat: Chat;
+    
+    function openChat() {
+        goto(`/chats/${chat.id}`)
+    }
+</script>
+
+<button on:click={openChat} class="flex flex-col px-2 my-1 border-t-2 border-black hover:bg-slate-400">
+    <div class="font-bold">{chat.title}</div>
+    <div>{chat.summary}</div>
+</button>
--- victoria-frontend/src/lib/components/lists/items/ChatListItem.test.ts
+++ victoria-frontend/src/lib/components/lists/items/ChatListItem.test.ts
@@ -1,9 +1,56 @@
 import { expect, test } from "vitest";
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/svelte';
+import { vi } from "vitest";
+import { render, waitFor } from '@testing-library/svelte';
+import ChatListItem from "./ChatListItem.svelte";
+import { goto } from "$app/navigation";
 
-test.todo('chat list item shows title of chat with given id')
+vi.mock("$app/navigation", () => ({
+    goto: vi.fn()
+}))
 
-test.todo('chat list item shows summary of chat with given id')
 
-test.todo('clicking chat list item routes to the chat view for the given chat')
+test('chat list item shows title of given chat', async () => {
+    const { baseElement } = render(ChatListItem, {
+        chat: {
+            id: 1,
+            title: "Cooking soup",
+            summary: "How do I cook soup?",
+            rootExchangeId: 1
+        }
+    });
+
+    expect(baseElement).toHaveTextContent("Cooking soup")
+})
+
+test('chat list item shows summary of given chat', async () => {
+    const { baseElement } = render(ChatListItem, {
+        chat: {
+            id: 10,
+            title: "Exterminating bugs",
+            summary: "How does one exterminate bugs?",
+            rootExchangeId: 1
+        }
+    });
+    
+    expect(baseElement).toHaveTextContent("How does one exterminate bugs?")
+})
+
+test('clicking chat list item routes to the chat view for the given chat', async () => {
+    const user = userEvent.setup();
+    const { getByRole } = render(ChatListItem, {
+        chat: {
+            id: 21,
+            title: "Not building shelves",
+            summary: "Help I accidentally built a shelf!",
+            rootExchangeId: 1
+        }
+    });
+
+    const button = getByRole('button');
+    user.click(button);
+
+    await waitFor(() => {
+        expect(goto).toBeCalledWith("/chats/21")
+    });
+})
