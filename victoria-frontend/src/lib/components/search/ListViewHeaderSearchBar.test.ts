86b27e82f81d5baec2cf5aaae1fc7fe4848cc154
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-03 21:16:44 +0200

    feat: implement ListViewHeaderSearchBar

--- victoria-frontend/src/lib/components/search/ListViewHeaderSearchBar.test.ts
+++ victoria-frontend/src/lib/components/search/ListViewHeaderSearchBar.test.ts
@@ -1,13 +1,43 @@
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/svelte';
+import { render, screen, waitFor } from '@testing-library/svelte';
+import ListViewHeaderSearchBar from "./ListViewHeaderSearchBar.svelte";
 
-test.todo('typing in search bar changes prompt variable')
+test('pressing enter in search bar executes submit callback', async () => {
+    const callback = vi.fn((val) => {});
+    const user = userEvent.setup();
+    const { getByRole } = render(ListViewHeaderSearchBar, {
+        onsubmit: callback
+    });
+    
+    const bar = getByRole('search');
 
-test.todo('pressing enter in search bar executes submit callback')
+    await user.click(bar);
+    await user.keyboard("abc{Enter}");
+    
+    expect(callback).toBeCalled();
+    expect(callback).toBeCalledWith("abc");
+})
 
 // The "debounce period" is the amount of time that has to pass after a key was released,
 // until the typing callback is called - the timer is reset when typing
-test.todo('typing in search bar faster than the debounce period DOES NOT execute typing callback')
+test('typing in search bar debounces', async () => {
+    const callback = vi.fn((val) => {});
+    const user = userEvent.setup();
 
-test.todo('typing in search bar executes the typing callback after the debounce period')
+    const { getByRole } = render(ListViewHeaderSearchBar, {
+        ontype: callback,
+        debounce: 30,
+    });
+
+    const bar = getByRole('search');
+    await user.click(bar);
+    await user.keyboard("abc");
+
+    expect(callback).not.toHaveBeenCalled();
+    
+    await waitFor(() => {
+        expect(callback).toBeCalled();
+        expect(callback).toBeCalledWith("abc");
+    }, {timeout: 60});
+})
