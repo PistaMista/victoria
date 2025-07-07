1b550b62db123dbbb160b5f398f67eae836df22b
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-07 21:28:21 +0200

    fix: button callback types

--- victoria-frontend/src/lib/components/buttons/DeleteButton.svelte
+++ victoria-frontend/src/lib/components/buttons/DeleteButton.svelte
@@ -2,7 +2,7 @@
     import { Button } from "flowbite-svelte";
     import { TrashBinSolid } from "flowbite-svelte-icons";
 
-    export let onclick: () => {};
+    export let onclick: () => void;
 </script>
 
 <Button on:click={onclick} class="w-full font-bold bg-red-600"><TrashBinSolid/>DELETE</Button>
--- victoria-frontend/src/lib/components/buttons/SaveButton.svelte
+++ victoria-frontend/src/lib/components/buttons/SaveButton.svelte
@@ -2,7 +2,7 @@
     import { Button } from "flowbite-svelte";
     import { FloppyDiskSolid } from "flowbite-svelte-icons";
     
-    export let onclick: () => {};
+    export let onclick: () => void;
 </script>
 
 <Button on:click={onclick} class="w-full font-bold bg-blue-600"><FloppyDiskSolid/>SAVE</Button>
