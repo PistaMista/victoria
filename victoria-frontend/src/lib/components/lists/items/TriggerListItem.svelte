ee72e7b5caa192353cbe235369793135477fad98
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-07 20:45:29 +0200

    feat: implement TriggerListItem

--- victoria-frontend/src/lib/components/lists/items/TriggerListItem.svelte
+++ victoria-frontend/src/lib/components/lists/items/TriggerListItem.svelte
@@ -1,13 +1,21 @@
-<div class="flex flex-col px-2 my-1 border-t-2 border-black hover:bg-slate-400">
+<script lang="ts">
+    import type { TriggerListItem } from "$lib/types/trigger";
+    import { goto } from "$app/navigation";
+    
+    export let trigger: TriggerListItem;
+
+    function openTriggerDetail() {
+        goto(`/admin/triggers/${trigger.id}`);
+    }
+</script>
+
+<button on:click={openTriggerDetail} class="flex flex-col px-2 my-1 border-t-2 border-black hover:bg-slate-400">
     <div class="flex flex-row">
         <div class="font-bold">
-            Discord message arrived
+            {trigger.name}
         </div>
         <div class="ml-auto font-bold">
-            WEBHOOK
+            {trigger.type.toUpperCase()}
         </div>
     </div>
-    <div>
-        URL: /hooks/danwkjdnkjnjj
-    </div>
-</div>
+</button>
