db5cacd640bdc33366c36436e63db184b5796d91
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-07 19:19:39 +0200

    feat: implement MonologueListItem

--- victoria-frontend/src/lib/components/lists/items/MonologueListItem.svelte
+++ victoria-frontend/src/lib/components/lists/items/MonologueListItem.svelte
@@ -1,21 +1,47 @@
-<script>
+<script lang="ts">
     import MonologueStatusIndicator from "$lib/components/indicators/MonologueStatusIndicator.svelte";
+    import type { Agent } from "$lib/types/agent";
+    import type { MonologueListItem } from "$lib/types/monologue";
+    import { goto } from "$app/navigation";
+    import { onMount } from "svelte";
+    import { getAgent } from "$lib/api/agents";
+    
+    function openMonologueDetail() {
+        goto(`/monologues/${monologue.id}`)
+    }
+    
+    export let monologue: MonologueListItem;
+    let agent: Agent | null = null;
+    let mounted: Boolean = false;
+
+    onMount(() => {
+        mounted = true;
+    })
+    
+    $: if (mounted) {
+        (async () => {
+            agent = await getAgent(monologue.agentId);
+        })();
+    }
 </script>
 
-<div class="flex flex-row px-2 my-1 border-t-2 border-black hover:bg-slate-400">
+<button on:click={openMonologueDetail} class="flex flex-row px-2 my-1 border-t-2 border-black hover:bg-slate-400">
     <div class="content-center">
-        <MonologueStatusIndicator/>
+        <MonologueStatusIndicator
+            status={monologue.status}
+            startTimestamp={monologue.startTimestamp}
+        />
     </div>
     <div class="grow mx-2 min-w-0 flex flex-col">
         <div class="font-bold">
-            Research thesis ideas
+            {monologue.title}
         </div>
         <div>
-            Searching the web for sources...
+            {monologue.summary}
         </div>
 
         <div>
-            Started by <span class="font-semibold text-blue-500">Researcher</span> (<span class="font-semibold text-orange-400">Timer</span>) 20m ago
+            Started by <span class="font-semibold text-blue-500">{agent?.name}</span> 20m ago
         </div>
     </div>
-</div>
+</button>
--- victoria-frontend/src/lib/components/lists/items/MonologueListItem.test.ts
+++ victoria-frontend/src/lib/components/lists/items/MonologueListItem.test.ts
@@ -1,11 +1,57 @@
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/svelte';
+import { render, screen, waitFor } from '@testing-library/svelte';
+import type { MonologueListItem as Monologue } from "$lib/types/monologue";
+import MonologueListItem from "./MonologueListItem.svelte";
+import { goto } from "$app/navigation";
 
-test.todo('monologue list item shows title of monologue with given id')
+vi.mock("$app/navigation", () => ({
+    goto: vi.fn()
+}))
 
-test.todo('monologue list item shows summary of monologue with given id')
+const testMonologue: Monologue = {
+    id: 2,
+    agentId: 1,
+    startTimestamp: 3000,
+    title: "Research reading list for learning electronics",
+    summary: "Thinking...",
+    status: 'PENDING'
+};
 
-test.todo('monologue list item shows assigned agent of monologue with given id')
+test('monologue list item shows title of given monologue', async () => {
+    const { container } = render(MonologueListItem, {
+        monologue: testMonologue
+    })
+    
+    expect(container).toHaveTextContent("Research reading list for learning electronics");
+})
 
-test.todo('clicking monologue list item routes to given monologue detail view')
+test('monologue list item shows summary of given monologue', async () => {
+    const { container } = render(MonologueListItem, {
+        monologue: testMonologue
+    })
+    
+    expect(container).toHaveTextContent("Thinking...");
+})
+
+test('monologue list item shows name of assigned agent of given monologue', async () => {
+    const { container } = render(MonologueListItem, {
+        monologue: testMonologue
+    })
+
+    await waitFor(() => {
+        expect(container).toHaveTextContent("Started by Cook");
+    })
+})
+
+test('clicking monologue list item routes to given monologue detail view', async () => {
+    const user = userEvent.setup();
+    const { getByRole } = render(MonologueListItem, {
+        monologue: testMonologue
+    })
+    const button = getByRole('button');
+
+    await user.click(button);
+
+    expect(goto).toBeCalledWith('/monologues/2');
+})
