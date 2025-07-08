897c18eb560e93f471c7bf769f0390e547c93ba3
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-08 13:10:35 +0200

    feat: implement BackButton

--- victoria-frontend/src/lib/components/buttons/BackButton.test.ts
+++ victoria-frontend/src/lib/components/buttons/BackButton.test.ts
@@ -1,7 +1,22 @@
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/svelte';
+import BackButton from "./BackButton.svelte";
+import { goto } from "$app/navigation";
 
-test.todo('back button routes to provided route on click', async () => {
-    assert.fail('not implemented')
+vi.mock("$app/navigation", () => ({
+    goto: vi.fn()
+}))
+
+test('back button routes to provided route on click', async () => {
+    const user = userEvent.setup();
+    const { getByRole } = render(BackButton, {
+        route: "/my/fancy/route"
+    });
+
+    const button = getByRole('button');
+
+    await user.click(button);
+    
+    expect(goto).toBeCalledWith("/my/fancy/route")
 })
