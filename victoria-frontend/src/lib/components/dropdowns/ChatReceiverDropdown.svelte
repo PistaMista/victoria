fe0e9a4f7e80ec94f4d82fd40fbada02fa15f8ef
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-09 00:09:22 +0200

    feat: implement ChatReceiverDropdown

--- victoria-frontend/src/lib/components/dropdowns/ChatReceiverDropdown.svelte
+++ victoria-frontend/src/lib/components/dropdowns/ChatReceiverDropdown.svelte
@@ -1,13 +1,33 @@
 <script lang="ts">
+    import { getChatReceivers } from "$lib/api/chatting";
     import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
     import { ChevronDownOutline } from "flowbite-svelte-icons";
+    import { onMount } from "svelte";
+    
+    export let chosenReceiver: string | null = null;
+
+    let receivers: string[] = [];
+    let dropdownOpen: boolean = false;
+
+    onMount(async () => {
+        receivers = await getChatReceivers();
+        chosenReceiver = receivers.at(0) ?? null;
+    })
 </script>
 
 <div {...$$restProps}>
-    <Button class="flex flex-row w-full">General<ChevronDownOutline class="w-6 h-6"/></Button>
-    <Dropdown>
-        <DropdownItem>General</DropdownItem>
-        <DropdownItem>Technical</DropdownItem>
-        <DropdownItem>Research</DropdownItem>
+    <Button class="flex flex-row w-full">{chosenReceiver ?? "Choose receiver..."}<ChevronDownOutline class="w-6 h-6"/></Button>
+    <Dropdown bind:open={dropdownOpen}>
+        {#each receivers as receiver}
+            <DropdownItem
+                aria-label={receiver}
+                on:click={() => {
+                    chosenReceiver = receiver;
+                    dropdownOpen = false;
+                }}
+            >
+                {receiver}
+            </DropdownItem>
+        {/each}
     </Dropdown>
 </div>
--- victoria-frontend/src/lib/components/dropdowns/ChatReceiverDropdown.test.ts
+++ victoria-frontend/src/lib/components/dropdowns/ChatReceiverDropdown.test.ts
@@ -1,15 +1,54 @@
 import { expect, test } from "vitest";
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/svelte';
+import { render, screen, waitFor } from '@testing-library/svelte';
+import ChatReceiverDropdown from "./ChatReceiverDropdown.svelte";
+import { listChatReceiversHandler } from "../../../mocks/handlers/chats";
 
 // WARNING:
 // Chat receivers are NOT an independent entity in the database - they are just a string
 // property of chat triggers
 
-test.todo('chat receiver dropdown shows default option as selected when first rendered')
+test('chat receiver dropdown starts with first option selected', async () => {
+    const { container } = render(ChatReceiverDropdown);
 
-test.todo('chat receiver dropdown shows all chat receivers when clicked')
+    await waitFor(() => {
+        expect(listChatReceiversHandler).toBeCalled();
+    })
 
-test.todo('chat receiver dropdown sets chat receiver variable when an option is clicked')
+    expect(container).toHaveTextContent('general');
+    expect(container).not.toHaveTextContent('research');
+})
 
-test.todo('chat receiver dropdown shows clicked option as selected')
+test('chat receiver dropdown shows all chat receivers when clicked', async () => {
+    const user = userEvent.setup();
+    const { getByRole, container } = render(ChatReceiverDropdown);
+
+    await waitFor(() => {
+        expect(listChatReceiversHandler).toBeCalled();
+    })
+    
+    const button = getByRole('button');
+    await user.click(button);
+
+    expect(container).toHaveTextContent('general');
+    expect(container).toHaveTextContent('research');
+})
+
+test('chat receiver dropdown closes and shows option when it is selected', async () => {
+    const user = userEvent.setup();
+    const { getByRole, container, findByLabelText } = render(ChatReceiverDropdown);
+
+    await waitFor(() => {
+        expect(listChatReceiversHandler).toBeCalled();
+    })
+    
+    const button = getByRole('button');
+    await user.click(button);
+
+    const option = await findByLabelText('research');
+    await user.click(option);
+
+    expect(option).not.toBeInTheDocument();
+    expect(container).toHaveTextContent('research');
+    expect(container).not.toHaveTextContent('general');
+})
