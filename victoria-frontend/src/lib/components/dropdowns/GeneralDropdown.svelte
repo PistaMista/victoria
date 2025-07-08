461d5a025a6bef1ec7ed9e21beaa63177f6ed4cf
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-08 15:49:40 +0200

    feat: implement GeneralDropdown

--- victoria-frontend/src/lib/components/dropdowns/GeneralDropdown.svelte
+++ victoria-frontend/src/lib/components/dropdowns/GeneralDropdown.svelte
@@ -1,13 +1,34 @@
+<script lang="ts" context="module">
+    export type Option = {
+        displayName: string,
+        ariaLabel: string,
+        value: any
+    };
+</script>
+
 <script lang="ts">
     import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
     import { ChevronDownOutline } from "flowbite-svelte-icons";
+    
+    export let options: Option[];
+    export let chosenOption: Option | null = null;
+    export let placeholder: string;
+    
+    let dropdownOpen: boolean = false;
+    
+    function chooseOption(option: Option) {
+        chosenOption = option;
+        dropdownOpen = false;
+    }
 </script>
 
 <div {...$$restProps}>
-    <Button class="flex flex-row w-full">IDENTITY<ChevronDownOutline class="w-6 h-6"/></Button>
-    <Dropdown>
-        <DropdownItem>Identity</DropdownItem>
-        <DropdownItem>JSON</DropdownItem>
-        <DropdownItem>XML</DropdownItem>
+    <Button class="flex flex-row w-full">{chosenOption?.displayName ?? placeholder}<ChevronDownOutline class="w-6 h-6"/></Button>
+    <Dropdown bind:open={dropdownOpen}>
+        {#each options as option}
+            <DropdownItem aria-label={option.ariaLabel} on:click={() => chooseOption(option)}>
+                {option.displayName}
+            </DropdownItem>
+        {/each}
     </Dropdown>
 </div>
--- victoria-frontend/src/lib/components/dropdowns/GeneralDropdown.test.ts
+++ victoria-frontend/src/lib/components/dropdowns/GeneralDropdown.test.ts
@@ -1,9 +1,65 @@
 import { expect, test } from "vitest";
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/svelte';
+import GeneralDropdown from "./GeneralDropdown.svelte";
 
-test.todo('general dropdown shows set options when clicked')
+test('general dropdown shows placeholder by default', async () => {
+    const { container } = render(GeneralDropdown, {
+        placeholder: "Select house...",
+        "aria-label": "Houses",
+        options: [
+            {displayName: "Thatched", ariaLabel: "Thatched", value: 1},
+            {displayName: "Shotgun", ariaLabel: "Shotgun", value: 2},
+            {displayName: "Tall", ariaLabel: "Tall", value: true}
+        ]
+    });
+    
+    expect(container).toHaveTextContent("Select house...");
+})
 
-test.todo('general dropdown sets option variable when option clicked')
+test('general dropdown closes and shows selected option when option is selected', async () => {
+    const user = userEvent.setup();
+    const { getByLabelText, getByRole, container } = render(GeneralDropdown, {
+        placeholder: "Select house...",
+        "aria-label": "Houses",
+        options: [
+            {displayName: "Thatched", ariaLabel: "Thatched", value: 1},
+            {displayName: "Shotgun", ariaLabel: "Shotgun", value: 2},
+            {displayName: "Tall", ariaLabel: "Tall", value: true}
+        ]
+    });
+    
+    const button = getByRole('button');
+    await user.click(button);
 
-test.todo('general dropdown shows clicked option as selected')
+    const option = getByLabelText('Shotgun');
+    await user.click(option);
+
+    expect(option).not.toBeInTheDocument();
+    expect(container).not.toHaveTextContent("Thatched");
+    expect(container).not.toHaveTextContent("Tall");
+
+    expect(container).toHaveTextContent("Shotgun");
+})
+
+
+test('general dropdown shows set options when clicked', async () => {
+    const user = userEvent.setup();
+    const { getByRole, container } = render(GeneralDropdown, {
+        placeholder: "Select house...",
+        "aria-label": "Houses",
+        options: [
+            {displayName: "Thatched", ariaLabel: "Thatched", value: 1},
+            {displayName: "Shotgun", ariaLabel: "Shotgun", value: 2},
+            {displayName: "Tall", ariaLabel: "Tall", value: true}
+        ]
+    });
+    
+    const button = getByRole('button');
+    await user.click(button);
+    
+    expect(container).toHaveTextContent("Thatched");
+    expect(container).toHaveTextContent("Shotgun");
+    expect(container).toHaveTextContent("Tall");
+})
+
