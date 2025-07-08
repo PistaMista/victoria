e365c013782a321c3abe65d48a78244a0d27b322
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-08 18:37:30 +0200

    feat: implement Toggle

---
+++ victoria-frontend/src/lib/components/toggles/Toggle.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { Checkbox } from "flowbite-svelte";
+    
+    export let label: string;
+    export let enabled: boolean = false;
+</script>
+
+<div class="flex flex-row m-1 bg-slate-200 rounded-md p-1">
+    <div class="font-bold grow">
+        {label}
+    </div>
+    <Checkbox bind:checked={enabled} {...$$restProps}/>
+</div>
