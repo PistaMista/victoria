fc44295b8ea4c15d1d4f7855ce32956af201194a
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-09 23:27:16 +0200

    fix: remove trivial test cases for sections

--- victoria-frontend/src/lib/components/sections/DetailViewSection.test.ts
+++ victoria-frontend/src/lib/components/sections/DetailViewSection.test.ts
@@ -2,6 +2,4 @@ import { expect, test } from "vitest";
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/svelte';
 
-test.todo('detail view section shows section title')
-
-test.todo('detail view section contains provided child components')
+// Nothing to test here
--- victoria-frontend/src/lib/components/sections/LabeledSetting.test.ts
+++ victoria-frontend/src/lib/components/sections/LabeledSetting.test.ts
@@ -2,6 +2,4 @@ import { expect, test } from "vitest";
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/svelte';
 
-test.todo('labeled setting shows setting label')
-
-test.todo('labeled setting displays given child component')
+// Nothing to test here
