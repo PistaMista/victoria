d7181c1d5e753c1b40d65de1c63e3bf7b26a687c
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-09 23:20:24 +0200

    feat: implement UserListItem

--- victoria-frontend/src/lib/components/lists/items/UserListItem.svelte
+++ victoria-frontend/src/lib/components/lists/items/UserListItem.svelte
@@ -1,12 +1,19 @@
-<script>
+<script lang="ts">
     import UserAvatar from "$lib/components/indicators/UserAvatar.svelte";
     import { Button } from "flowbite-svelte";
     import { CogSolid } from "flowbite-svelte-icons";
+    import { goto } from "$app/navigation";
+    import type { UserListItem } from "$lib/types/user";
+
+    export let user: UserListItem;
 </script>
 
 <div class="flex flex-row">
     <UserAvatar/>
-    <div class="mr-auto">Kryštof Albrecht</div>
-    <div class="rounded-md bg-blue-500 px-1 mr-1 font-bold text-white">ADMIN</div>
-    <Button class="w-6 h-6 p-0"><CogSolid/></Button>
+    <div class="mr-auto">{user.username}</div>
+    <div class="rounded-md bg-blue-500 px-1 mr-1 font-bold text-white">{user.role}</div>
+    <Button 
+        on:click={() => goto(`/admin/users/${user.id}/edit`)}
+        class="w-6 h-6 p-0"
+    ><CogSolid/></Button>
 </div>                        
--- victoria-frontend/src/lib/components/lists/items/UserListItem.test.ts
+++ victoria-frontend/src/lib/components/lists/items/UserListItem.test.ts
@@ -1,9 +1,44 @@
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/svelte';
+import type { UserListItem } from "$lib/types/user";
+import ItemComponent from "./UserListItem.svelte"
+import { goto } from "$app/navigation"
 
-test.todo('user list item shows name of given user')
+vi.mock("$app/navigation", () => ({
+    goto: vi.fn()
+}))
 
-test.todo('user list item shows role of given user')
+const testUser: UserListItem = {
+    id: 3,
+    username: "josh",
+    role: 'user'
+}
 
-test.todo('pressing configure button in user list item routes to detail of given user')
+test('user list item shows name of given user', async () => {
+    const { container } = render(ItemComponent, {
+        user: testUser
+    });
+
+    expect(container).toHaveTextContent("josh");
+})
+
+test('user list item shows role of given user', async () => {
+    const { container } = render(ItemComponent, {
+        user: testUser
+    });
+
+    expect(container).toHaveTextContent("user");
+})
+
+test('pressing configure button in user list item routes to detail of given user', async () => {
+    const user = userEvent.setup();
+    const { getByRole } = render(ItemComponent, {
+        user: testUser
+    });
+
+    const button = getByRole('button');
+    await user.click(button);
+
+    expect(goto).toBeCalledWith("/admin/users/3/edit")
+})
