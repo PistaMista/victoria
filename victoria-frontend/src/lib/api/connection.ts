5a3ca324f4d1126586f37c5078e9ebb59836c5db
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-09 11:28:23 +0200

    feat: implement ConnectionDetailView

---
+++ victoria-frontend/src/lib/api/connection.ts
@@ -0,0 +1,51 @@
+import { ConnectionListItem, type Connection } from "$lib/types/connection";
+import { Connection as ConnectionSchema, ConnectionListItem as ConnectionListItemSchema } from "$lib/types/connection";
+import type { Diff } from "$lib/types/diff";
+import { z } from "zod";
+
+
+export async function getConnection(id: number): Promise<Connection> {
+    const res = await fetch(`/api/connections/${id}`, {
+        method: 'GET'
+    });
+    const json = await res.json();
+    
+    return ConnectionSchema.parse(json);
+}
+
+// TODO: Streamline PUT API endpoints returning either modified object or bool
+// (or make them return nothing)
+export async function updateConnection(id: number, changes: Diff<Connection>): Promise<boolean> {
+    const res = await fetch(`/api/connections/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(changes)
+    });
+    const json = await res.json();
+
+    return z.boolean().parse(json);
+}
+
+export async function deleteConnection(id: number): Promise<boolean> {
+    const res = await fetch(`/api/connections/${id}`, {
+        method: 'DELETE'
+    });
+    const json = await res.json();
+
+    return z.boolean().parse(json);
+}
+
+export async function createConnection(newConnection: Connection): Promise<ConnectionListItem> {
+    const res = await fetch(`/api/connections`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newConnection)
+    });
+    const json = await res.json();
+
+    return ConnectionListItem.parse(json);
+}
--- victoria-frontend/src/lib/views/details/ConnectionDetailView.svelte
+++ victoria-frontend/src/lib/views/details/ConnectionDetailView.svelte
@@ -1,30 +1,81 @@
-<script>
+<script lang="ts">
+    import { createConnection, deleteConnection, getConnection, updateConnection } from "$lib/api/connection";
     import CreateButton from "$lib/components/buttons/CreateButton.svelte";
     import DeleteButton from "$lib/components/buttons/DeleteButton.svelte";
     import SaveButton from "$lib/components/buttons/SaveButton.svelte";
     import DetailViewSection from "$lib/components/sections/DetailViewSection.svelte";
+    import type { Connection } from "$lib/types/connection";
+    import { getDiff } from "$lib/types/diff";
     import DetailView from "$lib/views/DetailView.svelte";
     import { Input } from "flowbite-svelte";
+    import { onMount } from "svelte";
+
+    export let id: number | null;
+    
+    // TODO: Create some sort of Modifiable<T> to encapsulate this loaded/modified pattern?
+    let loadedConnection: Connection = {
+        id: 0,
+        name: "",
+        url: ""
+    };
+    let modifiedConnection: Connection = structuredClone(loadedConnection);
+    $: changes = getDiff(loadedConnection, modifiedConnection);
+
+
+    async function onSave() {
+        await updateConnection(loadedConnection.id, changes);
+    }
+    
+    async function onDelete() {
+        await deleteConnection(loadedConnection.id);        
+    }
+    
+    async function onCreate() {
+        await createConnection(modifiedConnection);
+    }
+    
+    onMount(async () => {
+        if (id !== null) {
+            loadedConnection = await getConnection(id);
+            modifiedConnection = structuredClone(loadedConnection);
+        }
+    })
 </script>
 
 <DetailView backRoute="/admin/settings/connections">
-    <DetailViewSection title="URL">
-        <Input/>
+    <DetailViewSection title="Name">
+        <Input
+            aria-label="Name"
+            bind:value={modifiedConnection.name}
+        />
     </DetailViewSection>
 
-    <DetailViewSection title="API Key">
-        <Input/>
+    <DetailViewSection title="URL">
+        <Input
+            aria-label="URL"
+            bind:value={modifiedConnection.url}
+        />
+    </DetailViewSection>
+
+    <DetailViewSection title="Delete connection">
+        <DeleteButton
+            aria-label="Delete connection"
+            onclick={onDelete}
+        />
     </DetailViewSection>
     
     <DetailViewSection title="Save connection">
-        <SaveButton/>
+        <SaveButton
+            aria-label="Save connection"
+            onclick={onSave}
+        />
     </DetailViewSection>
     
-    <DetailViewSection title="Delete connection">
-        <DeleteButton/>
-    </DetailViewSection>
     
     <DetailViewSection title="Create connection">
-        <CreateButton/>
+        <CreateButton
+            aria-label="Create connection"
+            onclick={onCreate}
+        />
     </DetailViewSection>
 </DetailView>
