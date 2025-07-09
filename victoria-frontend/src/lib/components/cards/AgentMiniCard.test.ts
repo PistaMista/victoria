a5eb6f493f51552ffedef8f9b93fdffe82be3a1d
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-09 23:44:27 +0200

    feat: implement AgentMiniCard

--- victoria-frontend/src/lib/components/cards/AgentMiniCard.test.ts
+++ victoria-frontend/src/lib/components/cards/AgentMiniCard.test.ts
@@ -1,9 +1,27 @@
 import { expect, test } from "vitest";
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/svelte';
+import AgentMiniCard from "./AgentMiniCard.svelte";
+import type { AgentListItem } from "$lib/types/agent";
 
-test.todo('agent mini card shows name of agent with given id', async () => {
+const testAgent: AgentListItem = {
+    id: 1,
+    name: "John",
+    status: 'BUSY'
+}
+
+test('agent mini card shows name of given agent', async () => {
+    const { container } = render(AgentMiniCard, {
+        agent: testAgent
+    });
+    
+    expect(container).toHaveTextContent("John");
 })
 
-test.todo('agent mini card shows status icon of agent with given id', async () => {
+test('agent mini card shows status of given agent', async () => {
+    const { container } = render(AgentMiniCard, {
+        agent: testAgent
+    });
+    
+    expect(container).toHaveTextContent("BUSY");
 })
