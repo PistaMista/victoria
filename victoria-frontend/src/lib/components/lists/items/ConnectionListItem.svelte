338790fbf49bd23f4c3545f35821707da847797a
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-09 11:45:53 +0200

    feat: implement ConnectionListItem

--- victoria-frontend/src/lib/components/lists/items/ConnectionListItem.svelte
+++ victoria-frontend/src/lib/components/lists/items/ConnectionListItem.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+    import type { ConnectionListItem } from "$lib/types/connection";
     import { Button } from "flowbite-svelte";
     import { CogSolid } from "flowbite-svelte-icons";
+    import { goto } from "$app/navigation";
+    
+    export let connection: ConnectionListItem;
+    
+    
 </script>
 
 <div class="flex flex-row">
-    <div class="mr-auto">Homelab</div>
-    <Button class="w-6 h-6 p-0"><CogSolid/></Button>
+    <div class="mr-auto">{connection.name}</div>
+    <Button 
+        on:click={() => { goto(`/admin/settings/connections/${connection.id}/edit`)}}
+        class="w-6 h-6 p-0"
+    ><CogSolid/></Button>
 </div>                        
