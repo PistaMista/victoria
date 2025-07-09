22c9dab14fd9ad5921913a85db865fc505ad40be
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-09 18:21:17 +0200

    feat: implement EventTemplateEditor

--- victoria-frontend/src/lib/components/editors/EventTemplateEditor.test.ts
+++ victoria-frontend/src/lib/components/editors/EventTemplateEditor.test.ts
@@ -1,13 +1,29 @@
 import { expect, test } from "vitest";
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/svelte';
+import EventTemplateEditor from "./EventTemplateEditor.svelte";
 
-test.todo('event template editor displays parser variables')
+test('event template editor displays parser variables', async () => {
+    const { container } = render(EventTemplateEditor, {
+        variables: [ "content", "replyExchange" ],
+        template: ""
+    });
 
-test.todo('event template editor displays inline error on invalid syntax')
+    expect(container).toHaveTextContent("$(content)");
+    expect(container).toHaveTextContent("$(replyExchange)");
+})
 
-test.todo('event template editor displays inline error when using undefined variable')
-
-test.todo('event template editor highlights error in the text')
-
-test.todo('event template editor allows editing template')
+test('event template editor displays error when using undefined variable', async () => {
+    const user = userEvent.setup();
+    const { getByRole, container } = render(EventTemplateEditor, {
+        variables: [ "content", "replyExchange" ],
+        template: ""
+    });
+    
+    const box = getByRole('textbox');
+    
+    await user.clear(box);
+    await user.type(box, "Hehehe, undefined variable $(carrot)");
+    
+    expect(container).toHaveTextContent("Error: undefined variable 'carrot'");
+})
