87960993cfd0dd1ee054970167150a4176a6cc4c
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-10 19:11:17 +0200

    feat: add Loaders to MonologueListView

--- victoria-frontend/src/lib/views/lists/MonologueListView.svelte
+++ victoria-frontend/src/lib/views/lists/MonologueListView.svelte
@@ -3,6 +3,7 @@
     import ListViewHeaderSearchBar from "$lib/components/search/ListViewHeaderSearchBar.svelte";
     import ListViewHeaderDropdown, { type Option } from "$lib/components/dropdowns/ListViewHeaderDropdown.svelte";
     import ListItemComponent from "$lib/components/lists/items/MonologueListItem.svelte";
+    import Loader from "$lib/components/placeholders/Loader.svelte";
     import type { MonologueListItem, MonologueStatus } from "$lib/types/monologue";
     import { onMount } from "svelte";
     import type { AgentListItem } from "$lib/types/agent";
@@ -10,7 +11,6 @@
     import type { TriggerListItem } from "$lib/types/trigger";
     import { getPermittedTriggers } from "$lib/api/triggers";
     import { getCurrentUserMonologues } from "$lib/api/monologues";
-    import { validationMessage } from "happy-dom/lib/PropertySymbol.js";
     
     let triggerOption: Option | null;
     let monologueStatusOption: Option | null;
@@ -23,14 +23,21 @@
     let triggers: TriggerListItem[] = [];
     let monologues: MonologueListItem[] = [];
     
-    onMount(async () => {
-        mounted = true;
+    let initPromise: Promise<void> = Promise.resolve();
+    let dataPromise: Promise<void> = Promise.resolve();
+    
+    async function init() {
         agents = await getCurrentUserAgents();
         triggers = await getPermittedTriggers();
+    }
+        
+    onMount(async () => {
+        mounted = true;
+        initPromise = init();
     })
     
     $: if (mounted) {
-        (async () => {
+        dataPromise = (async () => {
             let triggerId: number | null = triggerOption?.value;
             let status: MonologueStatus | null = monologueStatusOption?.value;
             let agent: number | null = assignedAgentOption?.value;
@@ -41,8 +48,12 @@
     }
 </script>
 
+<Loader
+    promise={initPromise}
+    pendingMessage="Initializing..."
+    rejectMessage="Failed to initialize monologue list"
+>
 <ListView>
-
     <svelte:fragment slot="header">
         <ListViewHeaderSearchBar
             ontype={(val) => searchQuery = val}
@@ -95,10 +106,17 @@
     </svelte:fragment>
 
     <svelte:fragment slot="items">
-        {#each monologues as monologue}
-            <ListItemComponent
-                {monologue}
-            />
-        {/each}
+        <Loader
+            promise={dataPromise}
+            pendingMessage="Loading monologues..."
+            rejectMessage="Failed to load monologues"
+        >
+            {#each monologues as monologue}
+                <ListItemComponent
+                    {monologue}
+                />
+            {/each}
+        </Loader>
     </svelte:fragment>
 </ListView>
+</Loader>
--- victoria-frontend/src/lib/views/lists/MonologueListView.test.ts
+++ victoria-frontend/src/lib/views/lists/MonologueListView.test.ts
@@ -19,8 +19,8 @@ test('monologue list view contains all monologue titles', async () => {
 
 test('monologue list view can filter by trigger', async () => {
     const user = userEvent.setup();
-    const { getByLabelText } = render(MonologueListView);
-    const dropdown = getByLabelText("Filter by trigger");
+    const { findByLabelText } = render(MonologueListView);
+    const dropdown = await findByLabelText("Filter by trigger");
 
     const button = within(dropdown).getByRole('button');
     await user.click(button);
@@ -28,8 +28,8 @@ test('monologue list view can filter by trigger', async () => {
     const pollOption = await within(dropdown).findByLabelText('Discord message received');        
     await user.click(pollOption);
 
-    expect(listMonologuesHandler).toBeCalledTimes(2);
-    expect((listMonologuesHandler as Mock).mock.calls[1][0].request.url).toContain("trigger=4");
+    expect(listMonologuesHandler).toBeCalledTimes(3);
+    expect((listMonologuesHandler as Mock).mock.calls[2][0].request.url).toContain("trigger=4");
     
     // Can stop filtering by trigger
     // await user.click(button);
@@ -37,14 +37,14 @@ test('monologue list view can filter by trigger', async () => {
     const anyOption = await within(dropdown).findByLabelText('Any');
     await user.click(anyOption);
 
-    expect(listMonologuesHandler).toBeCalledTimes(3);
-    expect((listMonologuesHandler as Mock).mock.calls[2][0].request.url).not.toContain("trigger=");
+    expect(listMonologuesHandler).toBeCalledTimes(4);
+    expect((listMonologuesHandler as Mock).mock.calls[3][0].request.url).not.toContain("trigger=");
 })
 
 test('monologue list view can filter by monologue status', async () => {
     const user = userEvent.setup();
-    const { getByLabelText } = render(MonologueListView);
-    const dropdown = getByLabelText("Filter by monologue status");
+    const { findByLabelText } = render(MonologueListView);
+    const dropdown = await findByLabelText("Filter by monologue status");
 
     const button = within(dropdown).getByRole('button');
     await user.click(button);
@@ -52,8 +52,8 @@ test('monologue list view can filter by monologue status', async () => {
     const runningOption = await within(dropdown).findByLabelText('Running');        
     await user.click(runningOption);
 
-    expect(listMonologuesHandler).toBeCalledTimes(2);
-    expect((listMonologuesHandler as Mock).mock.calls[1][0].request.url).toContain("monologueStatus=RUNNING");
+    expect(listMonologuesHandler).toBeCalledTimes(3);
+    expect((listMonologuesHandler as Mock).mock.calls[2][0].request.url).toContain("monologueStatus=RUNNING");
 
     // Can stop filtering by monologue status
     // await user.click(button);
@@ -61,14 +61,14 @@ test('monologue list view can filter by monologue status', async () => {
     const anyOption = await within(dropdown).findByLabelText('Any');
     await user.click(anyOption);
 
-    expect(listMonologuesHandler).toBeCalledTimes(3);
-    expect((listMonologuesHandler as Mock).mock.calls[2][0].request.url).not.toContain("monologueStatus=");
+    expect(listMonologuesHandler).toBeCalledTimes(4);
+    expect((listMonologuesHandler as Mock).mock.calls[3][0].request.url).not.toContain("monologueStatus=");
 })
 
 test('monologue list view can filter by assigned agent', async () => {
     const user = userEvent.setup();
-    const { getByLabelText } = render(MonologueListView);
-    const dropdown = getByLabelText("Filter by assigned agent");
+    const { findByLabelText } = render(MonologueListView);
+    const dropdown = await findByLabelText("Filter by assigned agent");
 
     const button = within(dropdown).getByRole('button');
     await user.click(button);
@@ -76,8 +76,8 @@ test('monologue list view can filter by assigned agent', async () => {
     const cookOption = await within(dropdown).findByLabelText('Cook');        
     await user.click(cookOption);
 
-    expect(listMonologuesHandler).toBeCalledTimes(2);
-    expect((listMonologuesHandler as Mock).mock.calls[1][0].request.url).toContain("assignedAgent=1");
+    expect(listMonologuesHandler).toBeCalledTimes(3);
+    expect((listMonologuesHandler as Mock).mock.calls[2][0].request.url).toContain("assignedAgent=1");
 
     // Can stop filtering by assigned agent
     // await user.click(button);
@@ -85,20 +85,20 @@ test('monologue list view can filter by assigned agent', async () => {
     const anyOption = await within(dropdown).findByLabelText('Any');
     await user.click(anyOption);
 
-    expect(listMonologuesHandler).toBeCalledTimes(3);
-    expect((listMonologuesHandler as Mock).mock.calls[2][0].request.url).not.toContain("assignedAgent=");
+    expect(listMonologuesHandler).toBeCalledTimes(4);
+    expect((listMonologuesHandler as Mock).mock.calls[3][0].request.url).not.toContain("assignedAgent=");
 })
 
 test('monologue list view can search for monologues', async () => {
     const user = userEvent.setup();
-    const { getByRole } = render(MonologueListView);
-    const searchBar = getByRole('search');
+    const { findByRole } = render(MonologueListView);
+    const searchBar = await findByRole('search');
 
     await user.click(searchBar);
     await user.keyboard("random");
 
     await waitFor(() => {
-        expect(listMonologuesHandler).toBeCalledTimes(2);
-        expect((listMonologuesHandler as Mock).mock.calls[1][0].request.url).toContain("searchQuery=random");
+        expect(listMonologuesHandler).toBeCalledTimes(3);
+        expect((listMonologuesHandler as Mock).mock.calls[2][0].request.url).toContain("searchQuery=random");
     })
 })
