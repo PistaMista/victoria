526a5b62e7aa4eba6c69d73bbe6c7ba215da1bc6
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-10 21:32:58 +0200

    feat: add Loader to ChatReceiverDropdown

--- victoria-frontend/src/lib/components/dropdowns/ChatReceiverDropdown.svelte
+++ victoria-frontend/src/lib/components/dropdowns/ChatReceiverDropdown.svelte
@@ -3,18 +3,30 @@
     import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
     import { ChevronDownOutline } from "flowbite-svelte-icons";
     import { onMount } from "svelte";
+    import Loader from "../placeholders/Loader.svelte";
     
     export let chosenReceiver: string | null = null;
 
     let receivers: string[] = [];
     let dropdownOpen: boolean = false;
-
-    onMount(async () => {
+    
+    let dataPromise: Promise<any> = Promise.resolve();
+    
+    async function load() {
         receivers = await getChatReceivers();
         chosenReceiver = receivers.at(0) ?? null;
+    }
+
+    onMount(() => {
+        dataPromise = load()
     })
 </script>
 
+<Loader
+    promise={dataPromise}
+    pendingMessage="Loading valid chat receivers..."
+    rejectMessage="Failed to load chat receivers"
+>
 <div {...$$restProps}>
     <Button class="flex flex-row w-full">{chosenReceiver ?? "Choose receiver..."}<ChevronDownOutline class="w-6 h-6"/></Button>
     <Dropdown bind:open={dropdownOpen}>
@@ -31,3 +43,4 @@
         {/each}
     </Dropdown>
 </div>
+</Loader>
