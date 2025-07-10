5a0cba3f0a44450dbbbbb90429d6abc67eb833e7
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-10 19:01:25 +0200

    feat: make Loader use #await

--- victoria-frontend/src/lib/components/placeholders/Loader.svelte
+++ victoria-frontend/src/lib/components/placeholders/Loader.svelte
@@ -9,15 +9,37 @@
     let resolved: boolean = false;
     let error: string | null = null;
     
-    promise
-        .then(() => {
-            resolved = true
-        })
-        .catch((e) => error = e.toString());
+    // promise
+    //     .then(() => {
+    //         resolved = true
+    //     })
+    //     .catch((e) => error = e.toString());
 </script>
 
-{#if resolved}
+{#await promise}
+    <div class="mx-auto my-2">
+        <div>
+            <div class="flex flex-row text-yellow-700 font-bold justify-center">
+                <Spinner color="yellow" class="mr-2"/> {pendingMessage}
+            </div>
+        </div>
+    </div>
+{:then _} 
     <slot/>
+{:catch err}
+    <div class="mx-auto my-2">
+        <div>
+            <div class="flex flex-row text-red-500 font-bold justify-center">
+                <CloseOutline class="mr-2"/> {rejectMessage}
+            </div>
+            <div class="text-red-500">
+                {error}
+            </div>
+        </div>
+    </div>
+{/await}
+
+<!-- {#if resolved}
 {:else}
     <div class="mx-auto my-2">
         <div>
@@ -35,4 +57,4 @@
             {/if}
         </div>
     </div>
-{/if}
+{/if} -->
