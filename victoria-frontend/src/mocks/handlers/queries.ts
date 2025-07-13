8dde5fd92c0c4696150ea1dcd38db7d0b11be53f
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-13 12:41:57 +0200

    feat: simplify /queries mock handler

--- victoria-frontend/src/mocks/handlers/queries.ts
+++ victoria-frontend/src/mocks/handlers/queries.ts
@@ -1,10 +1,37 @@
 import { http, HttpResponse } from "msw"
 import { spy } from "../spy"
 
-export const answerQueryHandler = await spy(() => {
-    return HttpResponse.json<boolean>(true);
+export const getAnsweredHandler = await spy(({params: { id }}) => {
+    switch (id) {
+        case '1':
+            return HttpResponse.json<any>(true);
+        case '2':
+            return HttpResponse.json<any>(false);
+        case '3':
+            return HttpResponse.json<any>(null);
+        case '4':
+            return HttpResponse.json<any>([1, 2, 3]);
+        default:
+            return HttpResponse.json<any>(null);
+    }
+})
+
+export const answerQueryHandler = await spy(({params: { id }}) => {
+    switch (id) {
+        case '1':
+            return HttpResponse.json<any>(true);
+        case '2':
+            return HttpResponse.json<any>(false);
+        case '3':
+            return HttpResponse.json<any>("lol");
+        case '4':
+            return HttpResponse.json<any>([1, 2, 3]);
+        default:
+            return HttpResponse.json<any>(null);
+    }
 })
 
 export const handlers = [
+    http.get('/queries/:id/answer', getAnsweredHandler),
     http.post('/queries/:id/answer', answerQueryHandler)
 ]
