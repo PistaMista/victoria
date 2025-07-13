ec0889d4294b53b5db077a2586fb616e1e5c6b27
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-13 13:58:55 +0200

    feat: implement ImageContent

--- victoria-frontend/src/lib/components/chat/messages/ImageContent.svelte
+++ victoria-frontend/src/lib/components/chat/messages/ImageContent.svelte
@@ -1,1 +1,7 @@
-<div class="w-2/3 mx-auto h-24 bg-red-500 rounded-md"/>
+<script lang="ts">
+    import type { ImageContent } from "$lib/types/message";
+
+    export let content: ImageContent;
+</script>
+
+<img src={content.imageDataURI} class="w-2/3 mx-auto h-24 bg-red-500 rounded-md"/>
