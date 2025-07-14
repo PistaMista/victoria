74ddcfee74d068d0587083395f5337a0e9d66905
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-14 18:04:27 +0200

    feat: add auth todo comment

--- victoria-frontend/src/routes/(app)/+layout.ts
+++ victoria-frontend/src/routes/(app)/+layout.ts
@@ -2,6 +2,8 @@ import { redirect } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 import { authToken } from '$lib/stores/auth';
 
+// TODO: Implement this entirely via HTTP-only cookies
+
 export async function load() {
     const token = get(authToken);
 
