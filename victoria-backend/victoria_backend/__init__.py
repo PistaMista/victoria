54ce428738bcf5310df6a8516b5acc389e7956fc
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-16 11:33:44 +0200

    feat: basic deployment using docker-compose

--- victoria-backend/victoria_backend/__init__.py
+++ victoria-backend/victoria_backend/__init__.py
@@ -1,15 +1,12 @@
 import os
-from time import sleep
 from victoria_backend.lib.spa_static_files import SPAStaticFiles
 from victoria_backend.api import api_router
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 frontend_path = os.environ.get('FRONTEND_PATH', '../victoria-frontend/build')
 index_path = os.path.join(frontend_path, 'index.html')
-port = os.environ.get('VICTORIA_PORT', 5001)
+port = int(os.environ.get('VICTORIA_PORT', 5001))
 address = os.environ.get('VICTORIA_ADDRESS', "127.0.0.1")
 app = FastAPI()
 
@@ -20,5 +17,4 @@ app.include_router(api_router, prefix="/api")
 app.mount("/", SPAStaticFiles(directory=frontend_path))
 
 def main():
-    print(frontend_path)
     uvicorn.run("victoria_backend:app", host=address, port=port, reload=True)
