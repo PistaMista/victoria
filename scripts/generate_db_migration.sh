d5fefc6b656c70b5bfd438e359e684e3659b1c8b
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-17 21:43:24 +0200

    fix: use poetry run in generate_db_migration.sh

--- scripts/generate_db_migration.sh
+++ scripts/generate_db_migration.sh
@@ -15,7 +15,8 @@ until docker exec $TEMP_DB_CONTAINER pg_isready -U user > /dev/null 2>&1; do
     sleep 0.5;
 done
 
-alembic -c victoria-backend/pyproject.toml upgrade head && 
-alembic -c victoria-backend/pyproject.toml revision --autogenerate -m "$1"
+cd victoria-backend
+poetry run alembic upgrade head && 
+poetry run alembic revision --autogenerate -m "$1"
 
 docker stop $TEMP_DB_CONTAINER
