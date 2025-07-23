c301f5eecb1a39afe0c143cf6b09fb5fa4d64603
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-23 20:25:31 +0200

    fix: migrations not generating

--- victoria-backend/alembic/env.py
+++ victoria-backend/alembic/env.py
@@ -11,11 +11,6 @@ from app.config import settings
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
 config.set_main_option('sqlalchemy.url', settings.DATABASE_URL)
 
 # add your model's MetaData object here
