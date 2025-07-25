b0632fa686d898c689c7e4e1bb532cd58a28351a
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-25 18:22:01 +0200

    fix: alembic migrations not working

--- victoria-backend/app/db/__init__.py
+++ victoria-backend/app/db/__init__.py
@@ -6,6 +6,6 @@ from app.config import settings
 def run_db_migrations():
     config = Config()
     parent_dir = os.path.dirname(__file__)
-    script_location = os.path.join(parent_dir, "../../alembic")
+    script_location = os.path.join(parent_dir, "../alembic")
     config.set_main_option('script_location', script_location)
     command.upgrade(config, "head")
