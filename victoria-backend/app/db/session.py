3f0d758aa28cb29a46bc289111002f2806145f8c
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-07-26 13:35:33 +0200

    feat: disable check_same_thread for db engine

--- victoria-backend/app/db/session.py
+++ victoria-backend/app/db/session.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
 if settings.DATABASE_URL:
-    engine = create_engine(settings.DATABASE_URL)
+    args = { "check_same_thread": False }
+    engine = create_engine(settings.DATABASE_URL, connect_args=args)
     Session = sessionmaker(engine)
 
 def get_db_session():
