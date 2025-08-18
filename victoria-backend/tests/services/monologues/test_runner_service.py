32508da7a67a7ccf293ca767c429a8465f32ba38
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-08-18 15:45:57 +0200

    feat: test for trying to run an already running monologue

--- victoria-backend/tests/services/monologues/test_runner_service.py
+++ victoria-backend/tests/services/monologues/test_runner_service.py
@@ -1,7 +1,8 @@
 import pytest
 from unittest import mock
-from threading import Event
-# from app.services.monologues.runner import RunnerService
+import threading
+from app.services.monologues.runner import RunnerService, MonologueThread, AlreadyRunningError
+from app.services.db import DatabaseService
 from app.model.agent import Agent
 from app.model.event import Event
 from app.model.monologue import Monologue, MonologueStatus
@@ -72,6 +73,7 @@ def test_runner_starts_new_thread_for_running_monologues(db_serv, db_session):
 
         # Assert
         MonologueThread.assert_called_once_with(7)
+        instance.start.assert_called_once()
 
 
 def test_runner_marks_running_monologues_as_running(db_serv, db_session):
@@ -239,6 +241,68 @@ def test_runner_ignores_finished_monologues(db_serv, db_session):
         assert success.status == MonologueStatus.SUCCESS    
         assert failure.status == MonologueStatus.FAILURE    
         MonologueThread.assert_not_called()
+        instance.start.assert_not_called()
+
+def test_runner_cannot_start_running_monologue(db_serv, db_session):
+    # Arrange
+    runner = RunnerService(
+        db_service=db_serv,
+        thread_limit=1
+    )
+    trigger = PollTrigger(
+        name="Trigger",
+        url="http://seznam.cz",
+        template="News entry added: (content)",
+        interval=1200
+    )
+    agent = Agent(
+        name="Reporter",
+        prompt="Notify the user of important world news",
+        allowed_triggers=[trigger]
+    )
+    event = Event(
+        content="News entry added: A cure for cancer discovered...",
+        trigger=trigger,
+        dispatched=True,
+        monologues=[]
+    )
+    trigger_invocation = TriggerInvocation(
+        event=event
+    )
+    trigger_thought = Thought(
+        invocation=trigger_invocation,
+        result="News entry added: A cure for cancer discovered..."
+    )
+    monologue = Monologue(
+        id=7,
+        title="None",
+        summary="None",
+        event=event,
+        agent=agent,
+        status=MonologueStatus.PENDING,
+        thoughts=[
+            trigger_thought
+        ]
+    )
+    event.monologues = [ monologue ]
+    
+    db_session.add(monologue)
+    db_session.commit()
+
+    with mock.patch("app.services.monologues.runner.MonologueThread") as MonologueThread:
+        instance = MonologueThread.return_value
+        instance._id = 7
+        instance.start.return_value = None
+
+        # Act
+        runner.start_monologue_process(7)
+        
+        with pytest.raises(AlreadyRunningError):
+            runner.start_monologue_process(7)
+
+        # Assert
+        MonologueThread.assert_called_once_with(7)
+        assert monologue.status == MonologueStatus.RUNNING
 
 def test_runner_queues_excess_monologues(db_serv, db_session):
     # Arrange
@@ -296,7 +360,7 @@ def test_runner_queues_excess_monologues(db_serv, db_session):
         runner.start_monologue_process(8)
 
         # Assert
-        MonologueThread.assert_called_once_with(7)
+        instance.start.assert_called_once()
         
         assert mon1.status == MonologueStatus.RUNNING
         assert mon2.status == MonologueStatus.PENDING
@@ -307,7 +371,7 @@ def test_runner_starts_first_queued_monologue_after_a_monologue_finishes(db_serv
         db_service=db_serv,
         thread_limit=1
     )
-    wait_event = Event()
+    wait_event = threading.Event()
     trigger = PollTrigger(
         name="Trigger",
         url="http://seznam.cz",
@@ -369,5 +433,7 @@ def test_runner_starts_first_queued_monologue_after_a_monologue_finishes(db_serv
         # Assert
         MonologueThread.assert_called_once_with(8)
         
-        assert mon1.status == MonologueStatus.SUCCESS
+        # ...threads that end without being marked as success are considered failed
+        # TODO: Assert this fact in a separate test
+        assert mon1.status == MonologueStatus.FAILURE
         assert mon2.status == MonologueStatus.RUNNING
