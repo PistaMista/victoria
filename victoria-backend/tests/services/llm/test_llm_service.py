bfefadf4f6d94747d38e29cbc850f856a4c20c6a
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-09-06 00:28:41 +0200

    feat: lay out tests for LLM service

---
+++ victoria-backend/tests/services/llm/test_llm_service.py
@@ -0,0 +1,31 @@
+import pytest
+
+def test_llm_service_can_add_a_new_ollama_connection():
+    assert False
+
+def test_llm_service_imports_models_from_added_ollama_connection():
+    assert False
+
+def test_llm_service_fails_to_add_ollama_connection_on_ollama_communication_failure():
+    assert False
+
+def test_llm_service_imports_models_from_existing_connections_on_startup():
+    assert False
+
+def test_llm_service_overrides_models_in_database_with_models_from_existing_connection():
+    assert False
+    
+def test_llm_service_keeps_models_in_database_on_ollama_communication_failure():
+    assert False
+
+def test_llm_service_can_remove_an_ollama_connection():
+    assert False
+
+def test_llm_service_removes_models_imported_from_a_removed_connection():
+    assert False
+
+def test_llm_service_calls_ollama_api_correctly_for_chat_completion():
+    assert False
+    
+def test_llm_service_does_not_catch_http_exceptions_during_chat_completion():
+    assert False
