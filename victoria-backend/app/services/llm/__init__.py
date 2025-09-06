c89fc0103099effa65c085a6bd830223c85ba33a
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-09-06 14:57:57 +0200

    feat: write tests for LLMService

--- victoria-backend/app/services/llm/__init__.py
+++ victoria-backend/app/services/llm/__init__.py
@@ -17,3 +17,19 @@ class AssistantMessage(Message):
 
 class UserMessage(Message):
     pass
+
+class NonexistentConnectionError(Exception):
+    def __init__(id: int):
+        super().__init__(f"the connection with id {id} does not exist")
+
+class NonexistentModelError(Exception):
+    def __init__(id: int):
+        super().__init__(f"the model with id {id} does not exist")
+
+class DisabledModelError(Exception):
+    def __init__(id: int):
+        super().__init__(f"the model with id {id} is disabled and cannot be used")
+
+class OllamaCommunicationError(Exception):
+    def __init__():
+        super().__init__("failed to communicate with Ollama server")
