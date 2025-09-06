fe8dab272c144588205562e8dd89917b0e41dcba
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-09-06 21:23:10 +0200

    feat: add function data to Action ORM model

--- victoria-backend/app/model/action.py
+++ victoria-backend/app/model/action.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Text, String, ForeignKey
+from sqlalchemy import Text, String, ForeignKey, JSON
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.model import Base
 from typing import List
@@ -9,4 +9,8 @@ class Action(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     function_name: Mapped[str] = mapped_column(String(80), nullable=False)
+    function_param_schema: Mapped[str] = mapped_column(JSON, nullable=False)
+    function_source_code: Mapped[str] = mapped_column(Text(), nullable=False)
+    function_docstring: Mapped[str] = mapped_column(Text(), nullable=False)
+
     allowed_on_agents: Mapped[List["Agent"]] = relationship("Agent", secondary=allowed_agent_action_association, back_populates="allowed_actions")
