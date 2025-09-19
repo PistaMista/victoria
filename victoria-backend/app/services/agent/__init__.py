c460ac6b6540bf0f945ef9e7e817ef92286d6989
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-09-19 10:46:38 +0200

    feat: add tests for /api/agents endpoint

--- victoria-backend/app/services/agent/__init__.py
+++ victoria-backend/app/services/agent/__init__.py
@@ -1,2 +1,20 @@
+from pydantic import BaseModel
+from typing import Optional, Dict, Any, List
+
 class AgentService:
     pass
+
+class AgentDiff(BaseModel):
+    name: Optional[str] = None
+    model_id: Optional[int] = None
+    model_params: Optional[Dict[str, Any]] = None
+    enabled_trigger_ids: Optional[List[int]] = None
+    enabled_action_ids: Optional[List[int]] = None
+
+class InvalidAgentSettingError(Exception):
+    def __init__(self, msg: str):
+        super().__init__(f"tried to set an invalid value for property of agent: {msg}")
+
+class NonexistentAgentError(Exception):
+    def __init__(self, id: int):
+        super().__init__(f"agent with ID {id} does not exist")
