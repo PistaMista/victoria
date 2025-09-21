cb7cfa45190973e9648b3a9ab3b203d164a834c3
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-09-21 21:08:21 +0200

    feat: add tests for /api/chats

--- victoria-backend/app/services/chat/__init__.py
+++ victoria-backend/app/services/chat/__init__.py
@@ -1,2 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
 class ChatService:
     pass
+
+class ChatOptionsDiff(BaseModel):
+    receiver: Optional[str] = None
+    enabled_action_ids: Optional[List[int]] = None
+
+class NonexistentChatError(Exception):
+    def __init__(self, id: int):
+        super().__init__(f"nonexistent chat with ID {id}")
