5cabdf10a3f889ec8ace49babb3dc0705ecb0884
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-09-24 12:12:27 +0200

    feat: tests for /api/queries

--- victoria-backend/app/services/chat/__init__.py
+++ victoria-backend/app/services/chat/__init__.py
@@ -15,3 +15,11 @@ class NonexistentChatError(Exception):
 class NonexistentExchangeError(Exception):
     def __init__(self, id: int):
         super().__init__(f"nonexistent exchange with ID {id}")
+
+class NonexistentMessageError(Exception):
+    def __init__(self, id: int):
+        super().__init__(f"nonexistent message with ID {id}")
+
+class QueryAlreadyAnsweredError(Exception):
+    def __init__(self, id: int):
+        super().__init__(f"query message with ID {id} was already answered")
