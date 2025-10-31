931c5d738569f15f65a3aff5cd0069aeba0ec006
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-10-31 21:33:17 +0100

    fix: dispatcher not starting monologue threads

--- victoria-backend/tests/services/monologues/test_dispatcher_service.py
+++ victoria-backend/tests/services/monologues/test_dispatcher_service.py
@@ -513,6 +513,43 @@ def test_dispatcher_instructs_runner_to_process_existing_unfinished_monologues(d
         
     # Assert
     runner.start_monologue_process.assert_called_once_with(45)
+
+def test_dispatcher_instructs_runner_to_process_monologue_just_added_by_new_event(db_session, runner, dispatcher, owner):
+    # Arrange
+    trigger = PollTrigger(
+        name="Emails", 
+        url="http://mycooldomain.com",
+        template="An email has arrived: (content)",
+        interval=600
+    )
+    agent = Agent(
+        id=75,
+        name="Secretary",
+        prompt="Manage the user's calendar and tasks",
+        owner=owner,
+        allowed_triggers=[trigger]
+    )
+    db_session.add(trigger)
+    db_session.add(agent)
+    db_session.commit()
+
+    # Act
+    dispatcher.start()
+    event = Event( 
+        id=42, 
+        content="An email has arrived: Hello, this is...", 
+        trigger=trigger, 
+        dispatched=False, 
+        monologues=[]
+    )
+    db_session.add(event)
+    db_session.commit()
+    dispatcher.stop()
+    
+    # Assert
+    assert event.dispatched
+    assert len(event.monologues) == 1
+    runner.start_monologue_process.assert_called_once_with(event.monologues[0].id)
         
 def test_dispatcher_does_nothing_for_finished_monologues(db_session, runner, dispatcher, owner):
     # Arrange
--- victoria-frontend/src/lib/components/chat/Exchange.svelte
+++ victoria-frontend/src/lib/components/chat/Exchange.svelte
@@ -1,109 +1,132 @@
 <script lang="ts">
-    import ItemComponent from "./Message.svelte";
-    import MonologueStatusIndicator from "../indicators/MonologueStatusIndicator.svelte";
-    import { writable, type Writable } from "svelte/store";
-    import { ShareAllOutline } from "flowbite-svelte-icons";
-    import type { Exchange } from "$lib/types/exchange";
-    import type { Message } from "$lib/types/message";
-    import { onDestroy, onMount } from "svelte";
-    import { duplicateChatToExchange, startReceivingMessages } from "$lib/api/chatting";
-    import ExchangeContextButton from "../buttons/ExchangeContextButton.svelte";
-    import type { Monologue } from "$lib/types/monologue";
-    import { getMonologue } from "$lib/api/monologues";
-    import { goto } from "$app/navigation";
-    import Loader from "../placeholders/Loader.svelte";
-    
-    export let exchange: Exchange;
-    
-    let agentMessages: Writable<Message[]> = writable([]);
-    let monologues: Monologue[] = [];
-    
-    let abortController: AbortController = new AbortController();
+	import ItemComponent from "./Message.svelte";
+	import MonologueStatusIndicator from "../indicators/MonologueStatusIndicator.svelte";
+	import { writable, type Writable } from "svelte/store";
+	import { ShareAllOutline } from "flowbite-svelte-icons";
+	import type { Exchange } from "$lib/types/exchange";
+	import type { Message } from "$lib/types/message";
+	import { onDestroy, onMount } from "svelte";
+	import {
+		duplicateChatToExchange,
+		startReceivingMessages,
+	} from "$lib/api/chatting";
+	import ExchangeContextButton from "../buttons/ExchangeContextButton.svelte";
+	import type { Monologue } from "$lib/types/monologue";
+	import { getMonologue } from "$lib/api/monologues";
+	import { goto } from "$app/navigation";
+	import Loader from "../placeholders/Loader.svelte";
 
-    let receivePromise: Promise<any> = Promise.resolve();
-    let getMonologuesPromise: Promise<any> = Promise.resolve();
-    let createNewChatPromise: Promise<any> = Promise.resolve();
+	export let exchange: Exchange;
 
-    async function loadMonologues() {
-        monologues = await Promise.all(exchange.monologueIds.map(id => (getMonologue(id))));
-    }
+	let agentMessages: Writable<Message[]> = writable([]);
+	let monologues: Monologue[] = [];
 
-    function createNewChatFromHere() {
-        createNewChatPromise = duplicateChatToExchange(exchange.chatId, exchange.id);
-    }
-    
-    onMount(() => {
-        getMonologuesPromise = loadMonologues();
-        receivePromise = startReceivingMessages(exchange.id, agentMessages, abortController.signal);
-    })
-    
-    onDestroy(() => {
-        abortController.abort("Exchange component destroyed");
-    })
+	let abortController: AbortController = new AbortController();
+
+	let receivePromise: Promise<any> = Promise.resolve();
+	let getMonologuesPromise: Promise<any> = Promise.resolve();
+	let createNewChatPromise: Promise<any> = Promise.resolve();
+
+	async function loadMonologues() {
+		monologues = await Promise.all(
+			exchange.monologueIds.map((id) => getMonologue(id)),
+		);
+	}
+
+	function createNewChatFromHere() {
+		createNewChatPromise = duplicateChatToExchange(
+			exchange.chatId,
+			exchange.id,
+		);
+	}
+
+	onMount(() => {
+		getMonologuesPromise = loadMonologues();
+		receivePromise = startReceivingMessages(
+			exchange.id,
+			agentMessages,
+			abortController.signal,
+		);
+	});
+
+	onDestroy(() => {
+		abortController.abort("Exchange component destroyed");
+	});
 </script>
 
 {#await receivePromise}
-<div class="w-full flex flex-col">
-    <!-- This is the user's Message -->
-    {#if exchange.userMessage !== null}
-        <div class="border-b-2 border-slate-500 bg-orange-200 rounded-t-md p-1">
-            <ItemComponent message={exchange.userMessage}/>
-        </div>
-    {/if}
-    <!-- These are the Message(s) sent by the Agent -->
-    <div class="{exchange.userMessage ? "" : "rounded-t-md"} flex flex-col space-y-2 border-b-2 border-slate-500 border-dashed bg-slate-100 p-1">
-        {#each $agentMessages as message}
-            <ItemComponent {message}/>
-        {/each}
-    </div>
-    <!-- Running monologue info -->
-    <Loader
-        promise={getMonologuesPromise}
-        pendingMessage="Fetching monologues..."
-        rejectMessage="Failed to get monologues"
-    >
-        <div class="flex flex-col space-y-2">
-            {#each monologues as monologue}
-                <div class="flex flex-row mt-1">
-                    <a 
-                        aria-label="Go to monologue"
-                        class="font-semibold hover:text-blue-500" 
-                        on:click={() => goto(`/monologues/${monologue.id}`)}
-                        href="/monologues/2"
-                    >{monologue.title}</a>
-                    <div class="ml-auto">
-                        <MonologueStatusIndicator
-                            horizontal
-                            status={monologue.status}
-                            startTimestamp={monologue.startTimestamp}
-                            displayRuntime
-                        />
-                    </div>
-                </div>
-            {/each}
-        </div>
-    </Loader>
-    <!-- These buttons are only shown if there is no Exchange at this level with a non-completed Monologue -->
-    <!-- Use ExchangeContextButton here -->
-    <Loader
-        promise={createNewChatPromise}
-        pendingMessage="Creating new chat from here"
-        rejectMessage="Failed to create new chat"
-    >
-        <div class="flex flex-row mt-1 space-x-1 content-center">
-            <ExchangeContextButton
-                onclick={createNewChatFromHere}
-                aria-label="Create new chat from here"
-            >
-                <ShareAllOutline/>
-            </ExchangeContextButton>
-        </div>
-    </Loader>
-</div>
+	<div class="w-full flex flex-col">
+		<!-- This is the user's Message -->
+		{#if exchange.userMessage !== null}
+			<div
+				class="border-b-2 border-slate-500 bg-orange-200 rounded-t-md p-1"
+			>
+				<ItemComponent message={exchange.userMessage} />
+			</div>
+		{/if}
+		<!-- These are the Message(s) sent by the Agent -->
+		<div
+			class="{exchange.userMessage
+				? ''
+				: 'rounded-t-md'} flex flex-col space-y-2 border-b-2 border-slate-500 border-dashed bg-slate-100 p-1"
+		>
+			{#each $agentMessages as message}
+				<ItemComponent {message} />
+			{/each}
+		</div>
+		<!-- Running monologue info -->
+		<Loader
+			promise={getMonologuesPromise}
+			pendingMessage="Fetching monologues..."
+			rejectMessage="Failed to get monologues"
+		>
+			<div class="flex flex-col space-y-2">
+				{#each monologues as monologue}
+					<div class="flex flex-row mt-1">
+						<a
+							aria-label="Go to monologue"
+							class="font-semibold hover:text-blue-500"
+							on:click={() =>
+								goto(
+									`/monologues/${monologue.id}`,
+								)}
+							href={`/monologues/${monologue.id}`}
+							>{monologue.title}</a
+						>
+						<div class="ml-auto">
+							<MonologueStatusIndicator
+								horizontal
+								status={monologue.status}
+								startTimestamp={monologue.startTimestamp}
+								displayRuntime
+							/>
+						</div>
+					</div>
+				{/each}
+			</div>
+		</Loader>
+		<!-- These buttons are only shown if there is no Exchange at this level with a non-completed Monologue -->
+		<!-- Use ExchangeContextButton here -->
+		<Loader
+			promise={createNewChatPromise}
+			pendingMessage="Creating new chat from here"
+			rejectMessage="Failed to create new chat"
+		>
+			<div class="flex flex-row mt-1 space-x-1 content-center">
+				<ExchangeContextButton
+					onclick={createNewChatFromHere}
+					aria-label="Create new chat from here"
+				>
+					<ShareAllOutline />
+				</ExchangeContextButton>
+			</div>
+		</Loader>
+	</div>
 {:catch}
-<Loader
-    promise={receivePromise}
-    pendingMessage="Receiving messages..."
-    rejectMessage="Failure while receiving messages"
-/>
+	<Loader
+		promise={receivePromise}
+		pendingMessage="Receiving messages..."
+		rejectMessage="Failure while receiving messages"
+	/>
 {/await}
+
