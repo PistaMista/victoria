fd981961065670e1d900a706de8a290ac514da13
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-10-31 12:41:03 +0100

    fix: Loader messages for LanguageModelToggle

--- victoria-frontend/src/lib/components/toggles/LanguageModelToggle.svelte
+++ victoria-frontend/src/lib/components/toggles/LanguageModelToggle.svelte
@@ -1,27 +1,35 @@
 <script lang="ts">
-    import { setModelEnabled } from "$lib/api/models";
-    import type { Model } from "$lib/types/model";
-    import { Checkbox } from "flowbite-svelte";
-    import Loader from "../placeholders/Loader.svelte";
-    
-    export let model: Model;
-    
-    let enablePromise: Promise<any> = Promise.resolve();
-    
-    function onCheckboxChange() {
-        enablePromise = setModelEnabled(model.id, model.enabled);
-    }
+	import { setModelEnabled } from "$lib/api/models";
+	import type { Model } from "$lib/types/model";
+	import { Checkbox } from "flowbite-svelte";
+	import Loader from "../placeholders/Loader.svelte";
+
+	export let model: Model;
+
+	let enablePromise: Promise<any> = Promise.resolve();
+
+	function onCheckboxChange() {
+		enablePromise = setModelEnabled(model.id, model.enabled);
+	}
 </script>
 
 <div class="flex flex-row bg-slate-500 rounded-md p-1">
-    <Loader
-        promise={enablePromise}
-        pendingMessage={model.enabled ? "Disabling model..." : "Enabling model..."}
-        rejectMessage={model.enabled ? "Failed to disable model" : "Failed to enable model"}
-    >
-        <div class="text-white mr-auto">
-            {model.name}
-        </div>
-        <Checkbox bind:checked={model.enabled} on:change={onCheckboxChange}/>
-    </Loader>
+	<Loader
+		promise={enablePromise}
+		pendingMessage={model.enabled
+			? "Enabling model..."
+			: "Disabling model..."}
+		rejectMessage={model.enabled
+			? "Failed to enable model"
+			: "Failed to disable model"}
+	>
+		<div class="text-white mr-auto">
+			{model.name}
+		</div>
+		<Checkbox
+			bind:checked={model.enabled}
+			on:change={onCheckboxChange}
+		/>
+	</Loader>
 </div>
+
