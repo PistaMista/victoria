7360efc8543d01e71beb4abe3984d816a2c634a3
Krystof Albrecht <krystofalbrechtus@gmail.com>
2025-11-06 15:08:40 +0100

    fix: frontend tests using millisecond timestamps

--- victoria-frontend/src/lib/components/chat/Message.test.ts
+++ victoria-frontend/src/lib/components/chat/Message.test.ts
@@ -6,55 +6,55 @@ import Component from "./Message.svelte";
 
 const userMessage: Message = {
 
-    id: 1,
-    timestamp: Date.UTC(2025, 1, 1, 20, 0, 0),
-    senderName: "Krystof",
-    content: {
-        type: 'markdown',
-        markdownText: "Hello!"
-    }
+	id: 1,
+	timestamp: Date.UTC(2025, 1, 1, 20, 0, 0) / 1000,
+	senderName: "Krystof",
+	content: {
+		type: 'markdown',
+		markdownText: "Hello!"
+	}
 };
 const agentMessage: Message = {
-    id: 2,
-    timestamp: Date.UTC(2024, 1, 2, 3, 4, 5),
-    senderName: "Librarian",
-    content: {
-        type: 'markdown',
-        markdownText: "Agent message!"
-    }
+	id: 2,
+	timestamp: Date.UTC(2024, 1, 2, 3, 4, 5) / 1000,
+	senderName: "Librarian",
+	content: {
+		type: 'markdown',
+		markdownText: "Agent message!"
+	}
 };
 
 test('user message shows name of user', async () => {
-    const { container } = render(Component, {
-        message: userMessage
-    });
+	const { container } = render(Component, {
+		message: userMessage
+	});
 
-    expect(container).toHaveTextContent("Krystof");
+	expect(container).toHaveTextContent("Krystof");
 })
 
 test('agent message shows name of agent', async () => {
-    const { container } = render(Component, {
-        message: agentMessage
-    });
+	const { container } = render(Component, {
+		message: agentMessage
+	});
 
-    expect(container).toHaveTextContent("Librarian");    
+	expect(container).toHaveTextContent("Librarian");
 })
 
 test('message shows full date of sending', async () => {
-    // Mock the current date
-    vi.spyOn(Date, 'now').mockReturnValue(Date.UTC(2025, 1, 3, 14, 19, 11));
+	// Mock the current date
+	vi.spyOn(Date, 'now').mockReturnValue(Date.UTC(2025, 1, 3, 14, 19, 11) / 1000);
 
-    const { container } = render(Component, {
-        message: userMessage
-    });
+	const { container } = render(Component, {
+		message: userMessage
+	});
 
-    expect(container).toHaveTextContent("2025/02/01 20:00");    
+	expect(container).toHaveTextContent("2025/02/01 20:00");
 })
 
 test('message shows text content', async () => {
-    const { container } = render(Component, {
-        message: userMessage
-    });
+	const { container } = render(Component, {
+		message: userMessage
+	});
 
-    expect(container).toHaveTextContent("Hello!");    
+	expect(container).toHaveTextContent("Hello!");
 })
